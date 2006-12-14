16240618cd50b7f0553af9465cfc3d0ac5b04240
amahboub <amahboub@305884b8-be16-0410-aa53-a3be2363cef1>
2006-12-14 13:05:26 +0000

    la version en travaux de normal.v
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/coqfinitgroup/vtm@80 305884b8-be16-0410-aa53-a3be2363cef1

--- normal.v
+++ normal.v
@@ -20,6 +20,7 @@ Require Import paths.
 Require Import connect.
 Require Import groups.
 
+
 Set Implicit Arguments.
 Unset Strict Implicit.
 Import Prenex Implicits.
@@ -30,113 +31,10 @@ Open Scope group_scope.
 
 Variables (G: finGroupType) (H K: set G).
 
-Hypothesis subgrp_H: group H.
-Hypothesis subgrp_K: group K.
+Hypothesis group_H: group H.
+Hypothesis group_K: group K.
 Hypothesis subset_HK: subset H K.
 
-
-(**********************************************************************)
-(*  Definition of the conjugate set xHx^-1,                           *)
-(*                                                                    *)
-(*      y in xHx^-1 iff x^-1yx is in H                                *)
-(**********************************************************************)
-
-(* le conjugué de y par x est dans H *)
-Definition conjsg x y := H (y ^g x).
-
-Theorem conjsgE : forall x y, conjsg x y = H (y ^g x).
-Proof. done. Qed.
-
-Theorem conjsg1: forall x, conjsg x 1.
-Proof. by move=> x; rewrite/conjsg conj1g; apply: group1. Qed.
-
-Theorem conjs1g: forall x, conjsg 1 x = H x.
-Proof. by move=> x; rewrite/conjsg conjg1. Qed.
-
-Theorem conjsg_inv: forall x y, conjsg x y -> conjsg x y^-1.
-Proof. move=> x y; rewrite/conjsg  conjg_invg; exact: groupVr. Qed.
-
-Theorem conjsg_conj: forall x y z, conjsg (x * y) z = conjsg y (z ^g x).
-Proof. by move=> x y z;rewrite/conjsg conjg_conj. Qed.
-
-Theorem conjsg_subgrp: forall x, group (conjsg x).
-Proof. 
-  move=> x.
-  apply/andP; split; first by apply: conjsg1.
-  apply/subsetP=> z; case/prodgP=> x1 x2 Hx1 Hx2 ->{z};rewrite /conjsg conjg_mul.
-  exact: groupM. 
-Qed.
-
-Theorem conjsg_image: forall y,
-  conjsg y =1 image (conjg y^-1) H.
-Proof.
-  move=> y x; rewrite {2} (_:x = conjg y^-1 (conjg y x )).
-    rewrite/conjsg image_f; by [rewrite /conjg | apply conjg_inj].
-  by rewrite conjgK.
-Qed.
-
-Theorem conjsg_inv1: forall x,
-  (conjsg x) =1 H -> (conjsg x^-1) =1 H.
-Proof. move=> x Hx y; by rewrite -conjs1g -(mulVg x) conjsg_conj Hx. Qed.
-
-Theorem conjsg_card: forall x,
-  card (conjsg x) = card H.
-Proof. move=>x; rewrite (eq_card (conjsg_image x)); apply card_image; exact: conjg_inj. Qed.
-
-
-
-(*********************************************************************)
-(*              Definition of the normaliser                         *)
-(*    the normaliser of H is the set of all elements x of G          *)
-(*    such that  xHx^-1 = H                                           *)
-(*********************************************************************)
-
-(*
-Definition normaliser x := 
-  subset G (fun z => (conjsg x z == H z)).
-*)
-Definition normaliser x := subset H (conjsg x).
-
-Theorem norm_conjsg : forall x,
-  normaliser x -> H =1 (conjsg x).
-Proof. 
-  move=> x Hx;apply/subset_cardP.
-  by symmetry;apply conjsg_card.
-  by done.
-Qed.
-
-
-Theorem normaliser_grp: group normaliser.
-Proof.
-  apply/andP; split; apply/subsetP => x Hx.
-    by rewrite /conjsg conjg1.
-  apply/subsetP => z Hz; case/prodgP: Hx => u v Hu Hv -> {x}.
-  by rewrite conjsg_conj; apply (subsetP Hv); rewrite -conjsgE -(norm_conjsg Hu z).
-Qed.
-
-
-Theorem subset_normaliser: subset H normaliser.
-Proof.
-apply/subsetP => x Hx;rewrite/normaliser;apply/subsetP => y Hy;rewrite/conjsg.
-repeat apply: groupM;gsimpl; exact: groupVr;gsimpl.
-Qed.
-
-Lemma normaliser_rcoset : closed (rcoset H) normaliser.
-Proof. apply closed_rcoset; [exact normaliser_grp | exact subset_normaliser]. Qed.
-
-Lemma normaliser_prodg : subset K normaliser -> prodg H K =1 prodg K H.
-Proof.
-move=> HK x; apply/idPn/idPn; case/prodgP => u v Hu Hv ->; apply/set0Pn; rewrite/setI.
- exists (conjg v u); rewrite/rcoset {2}/conjg ; gsimpl; rewrite Hv -conjsgE.
- by rewrite (subsetP (subsetP HK v _) u).
-exists u; rewrite /rcoset Hu -(invgK u) {2}(invgK u) -conjgE -conjsgE.
-by rewrite (subsetP (subsetP HK (u^-1) _) v) // groupV //.
-Qed.
-
-Lemma normaliser_prodg_grp : subset K normaliser -> group (prodg H K).
-Proof.
-move=> H1; apply prodC_group => //; exact: normaliser_prodg.
-Qed.
 
 (********************************************************************)
 (*              Definition of a normal set                          *)
@@ -146,7 +44,7 @@ Qed.
 (********************************************************************)
 
 
-Definition normal := subset K normaliser. 
+Definition normal := subset K (normaliser H). 
 
 Theorem normalP:
   reflect (forall x y,  K x -> H y -> H (y ^g x)) normal.
@@ -157,7 +55,7 @@ move => H0; apply/subsetP => x H1; apply/subsetP => y H2; exact: H0.
 Qed.
 
 Theorem conjsg_normal: forall x, 
-  normal -> K x -> conjsg x =1 H.
+  normal -> K x -> conjsg H x =1 H.
 Proof.
 move => x H0 H1 y; apply/idP/idP => H2; last exact: (normalP H0) => //.
   rewrite /conjsg in H2; rewrite -conjs1g -(mulgV x) conjsg_conj; apply: (normalP H0) => //.
@@ -176,21 +74,12 @@ Hypothesis subgrp_K: group K.
 Hypothesis subset_HK: subset H K.
 Hypothesis normal_HK: normal H K.
 
-Theorem eq_conjsg: forall a b (x: G),
-  a =1 b -> conjsg a x =1 conjsg b x.
-Proof. by move=> a b x H0 y; rewrite/conjsg H0. Qed.
-
 Theorem normal_subset: forall L,
-  group L -> subset H L -> subset L K -> normal H L.
-Proof.
-  move=> L Hl Hhl Hlk; apply/normalP=> x y Hx Hy;rewrite/conjg.
-  by apply: (normalP _ _ normal_HK); first apply: (subsetP Hlk).
-Qed.
+ (* group L -> subset H L ->*) subset L K -> normal H L.
+Proof. move=> L HLK; exact: subset_trans HLK _. Qed.
 
 Theorem normaliser_normal: normal H (normaliser H).
-Proof. apply/normalP => x y;move/subsetP => Hx Hy;rewrite -conjsgE; exact: Hx. Qed.
-
-
+Proof. exact: subset_refl. Qed.
 
 End NormalProp.
 
@@ -203,35 +92,27 @@ Hypothesis group_H: group H.
 
 Let group_normH := normaliser_grp H.
 
+(* a more appropriate pick for representative in a rcoset *)
 Definition rcoset_repr x :=
   if H x then 1 else
   if pick (rcoset H x) is Some y then y else 1.
 
-
-(* sûrement à mettre dans groups *)
-Lemma rcoset_trans1 : forall x y, rcoset H x y -> rcoset H x =1 rcoset H y.
-Proof. by move=> x y Hxy u; rewrite /rcoset -{1}(mulgKv y u) -mulgA groupMr. Qed.
-
-Lemma rcoset_trans1r : forall x y, 
-  rcoset H x y -> forall z, rcoset H z x = rcoset H z y.
-Proof. move=> x y Hxy z; rewrite !(rcoset_sym _ z) //; exact: rcoset_trans1. Qed.
-
-(* choisir x comme valeur par defaut aide pour le cas absurde *)
 Lemma rcoset_rcoset_repr : forall x, rcoset H x (rcoset_repr x).
 Proof.
 rewrite /rcoset_repr => x; case Hx: (H x); first by rewrite rcoset_sym // rcoset1 //.
 case pickP => // K; case/idP: {K}(K x); exact: rcoset_refl.
 Qed.
 
-(* si on choisis la valeur x par défaut dans le cas absurde, prouver celui là par 
-réécriture devient pénible... *)
+(* we could have chosen to set the default values at the identity in *)
+(* the definition of rcoset-repr but proving the following lemma by *)
+(* rewriting becomes then tedious *)
 Lemma rcoset_reprP : forall x y,
   reflect (rcoset_repr x = rcoset_repr y) (rcoset H x y).
 Proof.
 move => x y; apply: (iffP idP) => [Hxy | Dx].
-  rewrite /rcoset_repr (eq_pick (rcoset_trans1 Hxy)).
-  by rewrite -!(rcoset1 H) (rcoset_trans1r Hxy).
-by rewrite (rcoset_trans1r (rcoset_rcoset_repr y)) -Dx rcoset_rcoset_repr.
+  rewrite /rcoset_repr (eq_pick (rcoset_trans1 group_H Hxy)) -!(rcoset1 H).
+  by rewrite (rcoset_trans1r group_H Hxy).
+by rewrite (rcoset_trans1r group_H (rcoset_rcoset_repr y)) -Dx rcoset_rcoset_repr.
 Qed.
 
 Lemma rcoset_repr1 : rcoset_repr 1 = 1.
@@ -242,6 +123,7 @@ Proof.
 move=> x; rewrite -(rcoset1 H) rcoset_sym // -{1}rcoset_repr1; exact: rcoset_reprP.
 Qed.
 
+(* rcoset_repr is idempotent *)
 Lemma rcoset_repr_rcoset_repr : forall x, 
   rcoset_repr (rcoset_repr x) = rcoset_repr x.
 Proof. move=> x; symmetry; apply/rcoset_reprP; exact:rcoset_rcoset_repr. Qed.
@@ -256,6 +138,7 @@ Variables (G : finGroupType) (H : set G).
 Hypothesis group_H: group H.
 
 Let group_normH := normaliser_grp H.
+
 (*********************************************************************)
 (*                                                                   *)
 (*    Definition of the finite set of all the elements               *)
@@ -263,7 +146,7 @@ Let group_normH := normaliser_grp H.
 (*                                                                   *)
 (*********************************************************************)
 
-
+(* representative for cosets *)
 Definition coset_repr x := rcoset_repr H (x * (rcoset_repr (normaliser H) x)^-1).
 
 Lemma coset_reprE : forall x,
@@ -308,8 +191,12 @@ case/rcosetP : Hxy => z Hz ->; apply /rcoset_reprP =>//; apply /rcosetP; exists 
 by gsimpl.
 Qed.
 
+(* WARNING : here we introduce a fake dependancy on group_H because otherwise
+the canonical structure to be define would not identify the proof of (group H)
+ to be use and hense would become useless *)
 
-Definition coset_set : set G := fun x => coset_repr x == x.
+
+Definition coset_set : set G := (fun _ x => coset_repr x == x) group_H.
 
 Lemma coset_set_coset_repr : forall x, coset_set (coset_repr x).
 Proof.
@@ -365,7 +252,7 @@ Qed.
 
 Lemma coset_val : forall x, coset (val x) = x.
 Proof.
-move => x; case:x => {x} z; rewrite/coset_set => Hz ; apply/val_eqP => /=.
+move=> x; case:x => {x} z; rewrite/coset_set => Hz ; apply/val_eqP => /=.
 rewrite -(eqP Hz); apply/eqP; unlock coset =>/=; move :(coset_set_coset_repr z).
 rewrite/coset_set; exact: eqP.
 Qed.
@@ -381,7 +268,6 @@ Proof. move=> x; unlock coset; exact:norm_coset_repr. Qed.
 
 Notation Local N := (normaliser H).
 
-
 Lemma coset_mul_morph : forall x y,
     normaliser H x ->
   coset (x * y) = coset_mul (coset x) (coset y).
@@ -389,7 +275,7 @@ Proof.
 move=> x y Hx; rewrite /coset_mul; apply: val_inj.
 symmetry; rewrite val_coset; last by apply groupM =>//; apply: norm_val_coset.
 rewrite val_coset // /coset -!lock /=; apply /rcoset_reprP=> //.
-rewrite -mulgA /coset_repr; set z := y * _; rewrite (_ : y * _ = z); last 
+rewrite -mulgA /coset_repr; set z := y * _; rewrite (_ : y * _ = z); last first.
   by congr (y * _^-1); apply /rcoset_reprP=> //; rewrite /rcoset; gsimpl; rewrite groupV.
 rewrite /rcoset (norm_conjsg Hx) /conjsg /conjg -groupV //; gsimpl.
 rewrite -(mulgA _ _ z^-1) groupM //; last exact: rcoset_rcoset_repr.
@@ -403,7 +289,6 @@ move=> u; move: (valP u); rewrite /coset_set=> Hu; rewrite -(eqP Hu); exact: nor
 Qed.
 
 Hint Resolve normaliser_val.
-
 
 Lemma coset_unitP : forall u, coset_mul coset_unit u = u.
 Proof.
@@ -436,6 +321,7 @@ Proof. move=> x y Nx; rewrite coset_mul_morph //. Qed.
 Lemma val_coset1 : val (1:coset_sig) = 1.
 Proof. by rewrite/unitg /= /coset_unit val_coset ?rcoset_repr1 // group1. Qed.
 
+
 (*********************************************************************)
 (*              Definition of the quotient group                     *)
 (*    the quotient of K by H  is the set of all canonical            *)
@@ -446,11 +332,12 @@ Section OneQuotient.
 
 Variable K : set G.
 
-(* Ca c'est la definition plus simple, mais qui force la definition à être 
-le "vrai" quotient H/K car le rcoset_repr caché ote le controle sur le temoins dans H
- et donc demande l'hypothese H inclus dans K.
+(* The definition corresponding to textbooks (H/K) would be :
 Definition quotient : set coset_eqType := fun x => K (val x).
-*)
+but the rcoset_repr hidden inside gives no control on the choice of *)
+(* witnesses inside H and hence demands the hypothesis H included in *)
+  (**)
+(* K *)
 
 (* This is in fact HK/H *)
 Definition quotient : set coset_eqType := image coset K.
@@ -483,12 +370,11 @@ Variables (G G': finGroupType) (H : set G) (H': set G').
 Variable f : G -> G'.
 
 
-
 Definition morphism := forall x y, H x -> H y -> f (x * y) = (f x) * (f y).
 
 Hypothesis f_morph: morphism.
 
-Hypothesis subgrp_H: group H.
+Hypothesis group_H: group H.
 
 Lemma morph1 :  f 1 = 1.
 Proof. by apply: (mulg_injl (f 1)); rewrite -f_morph ?group1 // !mulg1. Qed.
@@ -503,8 +389,6 @@ Qed.
 Lemma morph_conjg : forall x y, H x -> H y -> f (x ^g y) = (f x) ^g (f y).
 Proof. move=> x y Hx Hy; rewrite/conjg -morphV // -!f_morph // ?groupM // groupV //. Qed.
 
-
-Hypothesis group_H : group H.
 
 Lemma morph_image_grp : group (image f H).
 Proof.
@@ -536,9 +420,251 @@ apply/imageP; exists (v ^g u); first by apply Hn. rewrite -morph_conjg //.
 move/subsetP:subset_KH => -> //.
 Qed.
 
+(* lemme restriction morphisme? *)
 
+Section Ker.
+
+Definition ker := setI (preimage f (set1 1)) H.
+(*Definition ker x:= (f x == 1) &&  H x. *)
+
+Lemma ker1 : forall x, ker x -> f x = 1.
+Proof. by move=> x; case/andP; move/eqP->. Qed.
+(*Proof. by rewrite/ker/setI/preimage=> x; move/andP=>[H1 H2]; rewrite (eqP H1). Qed.*)
+
+Lemma ker_grp : group ker.
+apply/andP;split.
+   by rewrite/ker/preimage/setI morph1 /= (group1 group_H) eq_refl.
+apply/subsetP=> x; move/prodgP=>[y1 y2]; rewrite/ker/setI/preimage.
+move/andP=>[Hfy1 Hy1]; move/andP=> [Hfy2 Hy2 ->].
+rewrite /ker/preimage/setI; apply/andP; split; last by apply groupM =>//.
+by apply/eqP; rewrite f_morph=> //; rewrite -(eqP Hfy1) -(eqP Hfy2); gsimpl.
+Qed.
+
+Lemma group_conjsg : forall x y, H x -> H y -> H (x ^g y).
+Proof. move=> x y Hx Hy; rewrite/conjg; rewrite ?groupM // groupV //. Qed.
+
+Lemma normal_ker : normal ker H.
+Proof.
+apply/normalP=> x y Hx; rewrite/ker/setI/preimage; move/andP=>[H1 H2].
+by rewrite morph_conjg // -(eqP H1) conj1g eq_refl group_conjsg //.
+Qed.
+
+ (* trivialg H := subset H (set1 1) *) 
+
+Lemma kerP : reflect (forall x y, H x -> H y -> (f x = f y -> x = y)) (subset ker (set1 1)).
+Proof.
+apply: (iffP idP); rewrite/ker/setI/preimage.
+ move/subsetP=> H1 x y Hx Hy Hxy.
+ apply (mulg_injl (y ^-1)); rewrite mulVg (eqP (H1 (y^-1 * x) _)) //.
+ rewrite (@groupM _ _ group_H y^-1 x) ?f_morph ?groupV // morphV // Hxy mulVg eq_refl //.
+move=> H1; apply/subsetP=> x; move/andP=>[Hx1 Hx]. 
+rewrite (H1 1 x) // ?group1 // morph1; exact (eqP Hx1).
+Qed.
+
+End Ker.
 
 End Morphism.
+
+Implicit Arguments quotient [G H].
+
+Section QuotientMorphism.
+
+Variables (GT1 GT2 : finGroupType) (f : GT1 -> GT2) (G H : set GT1).
+
+Hypothesis G_grp : group G.
+Hypothesis Hf : morphism G f.
+Hypothesis H_grp : group H.
+Hypothesis normal_HG : normal H G.
+Hypothesis H_ker : subset H (ker G f).
+
+Let sub_HG : subset H G.
+Proof. by apply/subsetP=> x; move/(subsetP H_ker); case/andP. Qed.
+
+Let GoverH := quotient H_grp G.
+
+Let GoverH_grp := group_quotient H_grp G_grp normal_HG.
+
+Notation Local Gquo := (coset_groupType H_grp).
+
+Let quotient_fun (x : Gquo) := f (val x).
+
+Let norm_K_val_coset : forall x, G x -> normaliser H (val (coset H_grp x)).
+Proof.
+move=> x Hx; apply: (subsetP normal_HG).
+rewrite val_coset; last exact: (subsetP normal_HG).
+have:= @rcoset_rcoset_repr _ H H_grp x; move/rcosetP=> [z Hz ->].
+by move/(subsetP sub_HG): Hz => Hz; rewrite groupM.
+Qed.
+
+Let G_coset_val : forall x, G x -> G (val (coset G_grp x)).
+Proof.
+move=> x Hx; rewrite val_coset; last by exact :(subsetP norm_HoverKer)=>//.
+move/rcosetP :(@rcoset_rcoset_repr _ _ K_grp x)=> [z Hz ->]; rewrite groupM //.
+by move/andP:Hz=>[_ Hz].
+Qed.
+
+Section QuotientMorphism.
+
+
+Section Isomporhism_theorems.
+
+
+Section First_theorem.
+
+
+Open Scope group_scope.
+ 
+Variables (G G': finGroupType) (H : set G).
+Variable f : G -> G'.
+
+Hypothesis group_H: group H.
+
+Hypothesis f_morph : morphism H f.
+
+Let K := ker H f.
+
+Let K_grp := ker_grp f_morph group_H.
+
+Print quotient.
+Let HoverKer := quotient K_grp H.
+
+Let norm_HoverKer := normal_ker f_morph group_H.
+
+
+Let HoverKer_grp := group_quotient K_grp group_H norm_HoverKer.
+
+Notation Local Gquo := (coset_groupType K_grp).
+
+Let fquo(x:Gquo) := f (val x).
+
+
+Let norm_K_val_coset : forall x, H x -> normaliser K  (val (coset K_grp x)).
+Proof.
+move=> x Hx; apply:(subsetP norm_HoverKer); rewrite -/K.
+rewrite val_coset; last by exact:(subsetP norm_HoverKer)=>//.
+move:(@rcoset_rcoset_repr _ K K_grp x); move/rcosetP=>[z Hz ->].
+by rewrite groupM //; move/andP:Hz=>[H1 H2].
+Qed. 
+
+
+Let H_coset_val : forall x, H x -> H  (val (coset K_grp x)).
+Proof.
+move=> x Hx; rewrite val_coset; last by exact :(subsetP norm_HoverKer)=>//.
+move/rcosetP :(@rcoset_rcoset_repr _ _ K_grp x)=> [z Hz ->]; rewrite groupM //.
+by move/andP:Hz=>[_ Hz].
+Qed.
+
+
+
+Lemma fquo_morph : morphism HoverKer fquo.
+Proof.
+move=>x y.
+move/imageP=>[x1 Hx1 ->];move/imageP=>[y1 Hy1 ->]; rewrite -/K.
+set cx := coset _ x1; set cy := coset _ y1.
+have Hcxcy :  normaliser K (val cx * val cy).
+ by rewrite groupM=>//; first exact: normaliser_grp; apply:norm_K_val_coset=>//.
+rewrite /fquo {1}/mulg /= /coset_mul val_coset -/K //.
+move/rcosetP:(@rcoset_rcoset_repr _ K K_grp (val cx * val cy))=>[z Hz ->].
+move/andP:Hz; rewrite/preimage; move=>[Hz1 Hz2].
+by rewrite !f_morph // -?(eqP Hz1) ?mul1g // /cx /cy ?groupM //; try apply: H_coset_val=> //.
+Qed.
+
+(* +  noyau trivial *)
+
+(* (preimage coset) definit une bijection entre les sous groupes de *)
+(* H/Ker et les sous groupes de H qui contiennent le ker *)
+
+
+End First_theorem.
+
+
+Section Third_theorem.
+
+Open Scope group_scope.
+ 
+Variables (GT : finGroupType) (G H K : set GT).
+
+Hypothesis G_grp : group G.
+Hypothesis H_grp : group H.
+Hypothesis K_grp : group K.
+
+Hypothesis subset_HK : subset H K.
+Hypothesis subset_KG : subset K G.
+
+Hypothesis normal_HG : normal H G.
+Hypothesis normal_KG : normal K G.
+
+Let KoverH := quotient H_grp K.
+Let GoverH := quotient H_grp G.
+
+
+Let normal_HK := normal_subset normal_HG subset_KG.
+
+Let KoverH_grp := group_quotient H_grp K_grp normal_HK.
+Let GoverH_grp := group_quotient H_grp G_grp normal_HG.
+
+Check morphism. 
+Let Hquo := coset_groupType H_grp.
+
+
+Lemma morphism_coset : morphism G (coset H_grp).
+Proof.
+move=> x y; do 2 move/(subsetP normal_HG)=> ?; exact: coset_morph. 
+Qed.
+
+Lemma normal_KoH_GoH : normal KoverH GoverH.
+Proof. exact: (morph_normal_im morphism_coset). Qed.
+
+Definition third_grp_iso (u : coset_groupType KoverH_grp) : coset_groupType K_grp :=
+  coset K_grp (val (val u)).
+
+Let f (u : Hquo) := coset K_grp (val u).
+
+Let f_morph : morphism GoverH f.
+
+Lemma morphism_third_grp_iso : morphism (quotient KoverH_grp GoverH) third_grp_iso.
+Proof.
+
+apply: fquo_morph.
+
+) : coset_groupType
+  KoverH_grp :=
+
+G_grp _ _ _. subset_KG normal_KG.
+have:= morph_normal_im morphism_coset G_grp subset_KG normal_KG.
+done.
+apply (@morph_normal_im  _ _ _ coset _ _ _ subset_HK normal_HK)
+
+apply/normalP=> x y; rewrite /KoverH /GoverH /quotient; set Hcoset := coset H_grp.
+move/imageP=> [x1 Hx1 ->]; move/imageP=> [x2 Hx2 ->].
+have : Hcoset x2 = 1.
+move=> i. rewrite/Hcoset.
+
+SearchAbout coset.
+have : (Hcoset x1)^-1 = Hcoset (x1 ^-1).
+rewrite /Hcoset=> i; rewrite
+SearchAbout coset_inv.
+
+
+rewrite /conjg.
+
+rewrite /invg /= /coset_inv /mulg /= /coset_mul /Hcoset !val_coset. !coset_mul_morph -/Hcoset.
+
+
+ -coset_mul_morph.
+rewrite
+rewrite /coset_mul.
+apply/imageP.
+
+
+ Print conjg. 
+SearchAbout coset.  
+rewrite /quotient; apply/imageP.
+
+
+
+End Isomporhism_theorems.
+
 
 Unset Implicit Arguments.
 
