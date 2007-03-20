79c1ae4eae87f42c9954f1ffcf6f8e7273d63c8a
bertot <bertot@305884b8-be16-0410-aa53-a3be2363cef1>
2007-03-20 07:55:12 +0000

    properties of signature have been moved to another file signperm.v This version has the definition of determinants and is halfway through the proof of multi-linearity.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/coqfinitgroup/vtm@199 305884b8-be16-0410-aa53-a3be2363cef1

--- determinant.v
+++ determinant.v
@@ -1,88 +1,70 @@
 Require Import ssreflect ssrbool funs eqtype ssrnat seq fintype paths.
-Require Import connect div groups group_perm zp.
+Require Import connect div groups group_perm zp signperm indexed_products.
 
-Section Determinant.
+Section determinant_context.
+Variables (d:finType)(R:Type)(plus mult : R -> R -> R)(zero one m_one : R).
 
-(* used as index set for matrices *)
+Definition b2R (b:bool) := if b then m_one else one.
 
+Definition determinant (range:set d)(a:d->d->R) : R :=
+  iprod R plus zero (perm_finType d)(perm range)
+  (fun sigma => (mult (b2R (perm_signature d sigma)))
+       (iprod R mult one d range (fun i => a i (fun_of_perm sigma i)))).
 
-Variables (d:finType)(R:Type)(plus mult : R -> R -> R)(a:d->d->R).
+Section R_props.
+Hypotheses (mult1x : forall x, mult one x = x)
+  (mult0x : forall x, mult zero x = zero)
+  (plus0x : forall x, plus zero x = x)
+  (plusA : forall x1 x2 x3, plus x1 (plus x2 x3) = plus (plus x1 x2) x3)
+  (plusC : forall x1 x2, plus x1 x2=plus x2 x1)
+  (multA : forall x1 x2 x3, mult x1 (mult x2 x3) = mult (mult x1 x2) x3)
+  (multC : forall x1 x2, mult x1 x2 = mult x2 x1)
+  (distrR : forall x1 x2 x3, mult (plus x1 x2) x3=plus (mult x1 x3)(mult x2 x3)).
 
-
-Definition zerolt2 : 0 < 2 := is_true_true.
-
-Definition z2 := zp_group zerolt2.
-
-Definition zero := zp0 zerolt2.
-
-Definition onelt2 : 1 < 2 := is_true_true.
-
-Definition one : z2 := @EqSig _ (fun m => m < 2) _ onelt2.
-
-Definition bool_to_z2 (b:bool) := if b then zero else one.
-
-Definition z2_to_bool (x:z2) := if x == zero then true else false.
-
-Section sign.
-
-Variable sigma : permType d.
-
-Definition inversion (k l :d) : z2 :=
- bool_to_z2 ((index l (enum d) < index k (enum d)) 
-   == (index (sigma l) (enum d) < index (sigma k) (enum d))).
-
-Definition signature :=
-  foldr (fun i x => foldr
-           (fun j y => 
-              add_zp zerolt2
-               (if index i (enum d) < index j (enum d) then
-                    inversion i j else zero) y)
-           x (enum d)) zero (enum d).
-
-End sign.
-
-Lemma foldr_step : forall (A B:eqType)(f: A -> B -> B) x s v,
-  foldr f v (Adds x s) = f x (foldr f v s).
-done.
+Lemma prod_multilinear :
+ forall (range:set d) (a b c:d->d->R) i (beta gamma:R),
+  range i ->
+  (forall k, setD1 range i k -> forall j, range j -> b k j = a k j /\ c k j = a k j) ->
+  (forall j, range j -> a i j = plus (mult beta (b i j))(mult gamma (c i j))) ->
+  forall sigma, perm range sigma ->
+    iprod R mult one d range (fun i => a i (fun_of_perm sigma i)) =
+    plus 
+     (mult beta (iprod R mult one d range (fun i => b i (fun_of_perm sigma i))))
+     (mult gamma (iprod R mult one d range (fun i => c i (fun_of_perm sigma i)))).
+move => range a b c i beta gamma Hr Hs Hc sigma Hp.
+have H2: range =1  setU (set1 i) (setD1 range i).
+move => x; apply/idP/idP.
+intros H; case H': (i==x); rewrite /setU /setD1; rewrite H'.
+ by apply/orP;left.
+by rewrite H.
+move => Hor; case/orP: Hor.
+ by move => Hx; rewrite -(eqP Hx).
+by move => Hand; case/andP: Hand.
+have F1: disjoint (set1 i)(setD1 range i).
+apply/set0Pn => x; move: x => [x H]; move/andP: H => [Hi Hd]; move/andP: Hd => [Hni _].
+by move/negP: Hni.
+have F2 : range (sigma i) by rewrite perm_closed.
+rewrite !(eq_iprod_set _ _ _ _ _ _ _ H2) !iprod_setU ?iprod_set1 ?Hc => //.
+have F3 : forall k, setD1 range i k -> b k (sigma k) = a k (sigma k).
+ move => k H.
+ have F4: range k by move/andP: H => [_ H].
+ have F5: range (sigma k) by rewrite perm_closed.
+ by case (Hs _ H _ F5).
+rewrite (eq_iprod_f _ _ _ _ _ _ _ F3).
+have F4 : forall k, setD1 range i k -> c k (sigma k) = a k (sigma k).
+ move => k H.
+ have F4: range k by move/andP: H => [_ H].
+ have F5: range (sigma k) by rewrite perm_closed.
+ by case (Hs _ H _ F5).
+rewrite (eq_iprod_f _ _ _ _ _ _ _ F4).
+by rewrite !multA distrR.
 Qed.
 
-Theorem all_zeros_foldr :
-forall (A B: eqType)(g:A->A->A)(z:A)(f:B->B->A),
-  (forall x, g z x = x) -> (forall i j, f i j = z) ->
-forall (l1 l2: seq B),
-  foldr (fun i a => foldr (fun j b => g (f i j) b) a l1) z l2 == z.
-Proof.
-move => A B g z f H H0 l0; elim l0 => /=; first by move => l; elim l.
-move: l0 => _ x l0 Hl0 l; elim l; first done.
-by move: l=>_ y l; move/eqP=>Hl; rewrite /= H0 H Hl; apply: Hl0 (Adds y seq0).
-Qed.
-
-Lemma sign_unit : signature (perm_unit d) == zero.
-Proof.
-rewrite /signature.
-apply (all_zeros_foldr (fzp 2) d (add_zp zerolt2) zero
- (fun i j => if index i (FinType.enum d) < index j (FinType.enum d)
-               then inversion (perm_unit d) i j else zero)).
-by rewrite /zero; apply unit_zp. 
-move => i j; rewrite /inversion.
-case: (index i (FinType.enum d)<index j (FinType.enum d)); last done.
-by rewrite /fun_of_perm !g2f eq_refl. 
-Qed.
-
-(* to prove this lemma, I will need to partition the set over which the
-sum is performed and show that two parts of the partition can be computed
-together, yielding differences that add up to 0, that all other parts of
-the partition add to the same value as for the initial permutation, except
-for a single value (inversion i j), which is always one.
-
-However, nothing has to be done to define summations over finite sets when
-the target type is an arbitrary associative commutative group.
-*)
-Lemma sign_transpose :
-  forall i j s, signature (perm_mul (transperm i j) s) == 
-       add_zp zerolt2 one (signature s).
-Proof.
-
-Admitted.
-
-
+Check prod_multilinear.
+Lemma determinant_multilinear :
+ forall (range:set d) (a b c:d->d->R) i (beta gamma:R),
+  range i ->
+  (forall k, range k -> ~i==k -> forall j, b k j = a k j /\ c k j = a k j) ->
+  (forall j, range j -> a i j = plus (mult beta (b i j))(mult gamma (c i j))) ->
+ determinant range a = plus (mult beta (determinant range b))
+    (mult gamma (determinant range c)).
