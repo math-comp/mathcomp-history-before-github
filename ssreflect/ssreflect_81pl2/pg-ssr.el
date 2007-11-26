a2a9eb16bac1d24300ae401d5d3ab659a844cfc5
amahboub <amahboub@305884b8-be16-0410-aa53-a3be2363cef1>
2007-11-26 08:02:50 +0000

    new pg mode
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/coqfinitgroup/trunk@602 305884b8-be16-0410-aa53-a3be2363cef1

---
+++ ssreflect/ssreflect_81pl2/pg-ssr.el
@@ -0,0 +1,32 @@
+;; Customization of PG for ssreflect syntax
+;; Assia Mahboubi 2007
+
+(defcustom coq-user-tactics-db
+   '(("have" "hv" "have # : #" t "have") 
+     ("congr" "con" "congr #" t "congr")
+     ("wlog" "wlog" "wlog : / #" t "wlog")
+     ("without loss" "wilog" "without loss #" t "without loss")
+     ("unlock" "unlock" "unlock #" t "unlock")
+     ("suffices" "suffices" "suffices # : #" t "suffices")
+     ("suff" "suff" "suff # : #" t "suff")) 
+   "Extended list of tactics, includings ssr and user defined ones")
+
+
+(defcustom coq-user-commands-db
+  '(("Unset Strict Implicit" "unsti" nil t "Strict\\s-+Implicit")
+    ("Prenex Implicits" "pi" "Prenex Implicits #" t "Prenex\\s-+Implicits")
+)
+   "Extended list of commands, includings ssr and user defined ones")
+
+(defcustom coq-user-tacticals-db
+  '(("last" "lst" nil t "last"))
+  "Extended list of tacticals, includings ssr and user defined ones")
+
+(defcustom coq-user-reserved-db
+  '("is" "nosimpl")
+  "Extended list of tacticals, includings ssr and user defined ones")
+
+(defcustom coq-user-solve-tactics-db
+  '(("done" nil "done" nil "done")
+    )
+   "Extended list of clsoing tactic(al)s, includings ssr and user defined ones")
