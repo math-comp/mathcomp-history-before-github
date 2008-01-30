88a1c955447062651d6224646e00f631c138e0d0
amahboub <amahboub@305884b8-be16-0410-aa53-a3be2363cef1>
2008-01-30 13:50:36 +0000

    reverting previous buggy customization..
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/coqfinitgroup/trunk@669 305884b8-be16-0410-aa53-a3be2363cef1

--- ssreflect/ssreflect_81pl2/pg-ssr.el
+++ ssreflect/ssreflect_81pl2/pg-ssr.el
@@ -41,6 +41,3 @@
   '(("done" nil "done" nil "done")
     )
    "Extended list of closing tactic(al)s, includings ssr and user defined ones")
-
-(defcustom coq-variable-highlight-enable ''nil
-   "Turns off the tentative highlight of variables")
