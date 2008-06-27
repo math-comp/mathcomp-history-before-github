f90eb9764a2e24f81857255e389d1bf22438ede5
asperti <asperti@305884b8-be16-0410-aa53-a3be2363cef1>
2008-06-27 13:32:23 +0000

    Added a weaker form of coprime_quotient_cent
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/coqfinitgroup/trunk@973 305884b8-be16-0410-aa53-a3be2363cef1

--- coprime_act.v
+++ coprime_act.v
@@ -241,6 +241,15 @@ rewrite groupMl ?cAx // memJ_norm ?(groupV, subsetP nHA) // Hy /=.
 by rewrite groupMr // conjVg groupV conjgM -mem_conjg -def_Ax memJ_conjg.
 Qed.
 
+Lemma coprime_quotient_cent_weak : forall A G H,
+    H <| G -> A \subset 'N(H) -> coprime #|G| #|A| -> solvable G ->
+  'C_G(A) / H = 'C_(G / H)(A / H).
+move=> A G H normH nHA co so.
+move: (normH); case/normalisubP=> sHG nHG.
+apply: coprime_quotient_cent; rewrite //=; last by apply: (solvable_sub sHG so).  
+by rewrite -(LaGrange sHG) coprime_mull in co; move: co; case/andP.
+Qed.
+
 Lemma coprime_comm_normal_part : forall A G K,
   A \subset 'N(G) -> coprime #|G| #|A| -> solvable G ->
   hall_for (predC pi) G K -> K \subset 'C_G(A) ->
