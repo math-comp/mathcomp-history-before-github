dcaf031681e4e1ddb1766d4716b6c958a3ea3bce
gonthier <gonthier@305884b8-be16-0410-aa53-a3be2363cef1>
2008-07-15 10:01:32 +0000

    minor improvements
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/coqfinitgroup/trunk@981 305884b8-be16-0410-aa53-a3be2363cef1

--- coprime_comm.v
+++ coprime_comm.v
@@ -10,18 +10,10 @@ Import Prenex Implicits.
 Import GroupScope.
 Variable T : finGroupType.
 
-Lemma sub_gmul: forall (G H K : group T), G \subset 'N(K) -> H \subset 'N(K) ->
-G/K  \subset H/K -> G \subset K * H.
-move=> G H K normG normH.
-rewrite quotientE; rewrite quotientE; move/subsetP=> sub; apply/subsetP=> g gin.
-have exh: exists2 h : T, h \in 'N(K) :&: H & coset_of K g = coset_of K h.
-  apply/imsetP; apply: sub; apply/imsetP; exists g; rewrite //.
-  by apply/setIP; split; rewrite //=; apply: (subsetP normG).
-case exh => h; case/setIP => inNK inH coseteq.
-rewrite -mem_rcosets; apply/rcosetsP; exists h; first done. 
-rewrite -(coset_ofN inNK); rewrite -(coset_ofN (subsetP normG _ gin)).
-by congr val.
-Qed.
+
+Lemma quotientSK : forall G H K : {group T}, G \subset 'N(K) ->
+  (G / K \subset H / K) = (G \subset K * H).
+Proof. by move=> *; rewrite morphimSK ?ker_coset. Qed.
 
 (* same as the following but with weaker assumptions 
 Lemma comm_center_prod: forall (A G: group T),
@@ -42,40 +34,36 @@ apply/eqP; rewrite eqset_sub; apply/andP; split.
 Qed.
 *)
 
-Lemma comm_center_prod: forall (A G: group T),
+Lemma comm_center_prod : forall A G : {group T},
   A \subset 'N(G) -> coprime #|G| #|A| -> solvable G ->
   G = [~: G, A] * 'C_G(A) :> set _.
 Proof.
 move=> A G AsubNG coprime solv.
-apply/eqP; rewrite eqset_sub; apply/andP; split.
-- apply: sub_gmul; rewrite /=; first by apply:normGR.
-  - by apply: (subset_trans _ (normGR G A)); rewrite /centraliser; apply: subsetIl.
-  - rewrite coprime_quotient_cent_weak //=.
-    - by rewrite subsetI; apply/andP; split; last by apply: center_commgr.
-    - apply/andP; rewrite //=; split; last by apply:normGR.
-      by rewrite commsgC subcomm_normal //.
-    - by rewrite commsgC; apply: normGR.
-- rewrite -{3}mulGid; apply:imset2S; last by apply: subsetIl.
-  by rewrite commsgC subcomm_normal //.
+apply/eqP; rewrite eqset_sub andbC -{3}mulGid imset2S ?subsetIl //; last first.
+  by rewrite commsgC subcomm_normal.
+rewrite -quotientSK ?normGR // coprime_quotient_cent_weak //=; last first.
+- by rewrite commsgC normGR.
+- by rewrite [_ <| G]andbC normGR commsgC subcomm_normal.
+by rewrite subsetI andbC center_commgr /=.
 Qed.
 
-Lemma commGAA:   forall (A G: group T),
+Lemma commGAA:   forall A G : {group T},
   A \subset 'N(G) -> coprime #|G| #|A| -> solvable G ->
   [~: G, A] = [~: G, A, A].
-move=> A G AsubNG coprime solv.
+Proof.
+move=> A G AsubNG coGA solG.
 apply/eqP; rewrite eqset_sub; apply/andP; split; last first.
-Search [subset commutator].
-- by apply:commSg; rewrite commsgC subcomm_normal //.
-- rewrite {1}(comm_center_prod AsubNG coprime solv).
-  rewrite distr_sgcomm /=.
-  - by rewrite triv_comm_centr mulg1; apply: subset_refl.
-  - apply: (subset_trans (subsetIl G 'C(A))); apply: normGR.
-  - apply: (subset_trans (subsetIr G 'C(A))); apply cent_subset.
+  by rewrite commsgC subcomm_normal /= commsgC normGR.
+rewrite {1}(comm_center_prod AsubNG coGA solG) distr_sgcomm //=.
+- by rewrite triv_comm_centr mulg1 subset_refl.
+- by rewrite subIset // normGR.
+by rewrite subIset // orbC cent_subset.
 Qed.
 
-Lemma comm_center_triv:   forall (A G: group T),
+Lemma comm_center_triv:   forall A G : {group T},
   A \subset 'N(G) -> coprime #|G| #|A| -> abelian G ->
  [~: G, A] :&: 'C_G(A) = 1.
+Proof.
 move => A G norm co abel.
 pose toG x : {x' | x' \in G} := insubd (Sub 1 (group1 G)) x.
 have valG: {in G, cancel toG val} by move=> *; exact: insubdK.
