30f2220bff9ab92754cd6874a90f3873a039c8a8
garillot <garillot@305884b8-be16-0410-aa53-a3be2363cef1>
2008-11-19 18:38:53 +0000

    more cleanup, full structure for decreasing subfunctors of I
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/coqfinitgroup/trunk@1123 305884b8-be16-0410-aa53-a3be2363cef1

--- groupf.v
+++ groupf.v
@@ -35,39 +35,65 @@ Definition resp (F : obmap): Prop :=
   forall gT hT (G:{group gT}) (phi:{morphism G >-> hT}),
     phi @* (F _ G) \subset F _ (phi @* G).
 
-(* Functoriality on grp whose arrows are restricted to automorphisms *)
+(* Functoriality on grp whose arrows are restricted to isomorphisms *)
 
 Definition aresp (F : obmap) : Prop :=
-  forall gT (G:{group gT}) phi (HA:phi \in Aut G), 
-        (autm_morphism HA) @* (F _ G) \subset F _ ((autm_morphism HA) @* G).
+  forall gT hT (G:{group gT}) (phi:{morphism G >-> hT}) (HA:'injm phi), 
+    phi @* (F _ G) \subset F _ (phi @* G).
 
 Lemma aresp_of_resp : forall (F:obmap), resp F -> aresp F.
-Proof. by move=> F H gT G phi Ha; apply:H. Qed.
+Proof. by move=> F H gT hT G phi Ha; apply:H. Qed.
 
 (* "Anti-monotonicity" *)
 
 Definition decr (F : obmap) : Prop :=
   forall gT (H G : {group gT}), (H \subset G) -> (F _ G) :&: H \subset (F _ H).
 
+(* Modulo Application *)
+
+Definition appmod (F1 F2 : obmap) :=
+  fun gT (G:{set gT}) => coset (F2 gT G) @*^-1 (F1 _ (G / (F2 gT G))).
+
+(* Strong functoriality (wrt domains)*)
+
+Definition dresp (F : obmap): Prop :=
+  forall gT hT (G D:{group gT}) (phi:{morphism D >-> hT}),
+    phi @* (F _ G) \subset F _ (phi @* G).
+
+Lemma decr_of_dresp (sF:obmap) : dresp sF -> decr sF.
+Proof.
+move=> sF Hresp gT H G sHG; move: (Hresp _ _ G _ (idm_morphism H)) =>/=.
+rewrite -morphimIdom -[idm H @* G]morphimIdom !morphim_idm ?subsetIl //.
+by rewrite (setIidPl sHG) setIC.
+Qed.
+
+
 End IdentitySubFunctorDefs.
 
 Module BaseIdSubFunctor.
 
-(* Those mappings only functorial w.r.t. automorphisms*)
+(* Those mappings only functorial w.r.t. isomorphisms*)
 
 Implicit Types gT: finGroupType.
 
 Structure mixin (Fobj : obmap) : Type := Mixin { 
-(*)  _ : forall gT (G : {group gT}), group_set (Fobj gT G);*)
+  (* group preservation *)
+  _ : forall gT (G:{group gT}), group_set (Fobj gT G);
+  (* submapping *)
   _ : forall gT (G: {group gT}), Fobj gT G \subset G;
+  (* functoriality condition *)
   _ : aresp Fobj}.
 
 Structure class : Type := Class { Fobj :> obmap ; 
    _ : mixin Fobj}.
 
 Definition subset_of (Fobj : obmap) (MF : mixin Fobj) := 
-  let: Mixin s _ := MF return
+  let: Mixin _ s _ := MF return
   forall gT (G: {group gT}), Fobj gT G \subset G in s.
+
+Definition group_of (Fobj : obmap) (MF : mixin Fobj) := 
+  let: Mixin g _ _ := MF return
+  forall gT (G: {group gT}), group_set (Fobj gT G) in g.
 
 Coercion mixin_of F := let: Class _ m := F return mixin F in m.
 
@@ -77,8 +103,8 @@ Notation bisFc := BaseIdSubFunctor.class.
 Notation BisFc := BaseIdSubFunctor.Class.
 Notation BisFMixin := BaseIdSubFunctor.Mixin.
 
-Definition mkBaseisFc F Fsub Fresp := 
-  BisFc (@BisFMixin F Fsub Fresp).
+Definition mkBaseisFc F Fgrp Fsub Fresp := 
+  BisFc (@BisFMixin F Fgrp Fsub Fresp).
 
 Notation "[ 'bisFc' 'of' F ]" :=
   (match [is F : _ -> _ <: bisFc] as s return {type of BisFc for s} -> _ with
@@ -88,16 +114,17 @@ Notation "[ 'bisFc' 'of' F ]" :=
 Notation Local "''e_' s ( G )" := (s _ G)
   (at level 8,s at level 2, format "''e_' s ( G )") : group_scope.
 
+Lemma bisfc_groupset : forall (sF:bisFc) (gT:finGroupType) (G:{group gT}),
+  group_set (sF gT G).
+Proof. by move=> sF gT G; case sF=> F; case. Qed.
+
+Canonical Structure bisfc_group (sF:bisFc) (gT:finGroupType) (G:{group gT}) :=
+  (group (bisfc_groupset sF G)).
+
 Section BaseIdentitySubfunctorProps.
 
-Variables gT hT:finGroupType.
+Implicit Types gT hT:finGroupType.
 Variable sF: bisFc.
-Implicit Types G H : {group gT}.
-
-(*
-Canonical Structure bisfc_group G := 
-  Group (bisfc_groupset G).
-*)
 
 Lemma bisfc_clos : forall (fT:finGroupType) (H:{group fT}),
   'e_sF(H) \subset H.
@@ -106,25 +133,67 @@ Proof. by case sF=> F; case. Qed.
 Lemma bisfc_aresp : aresp sF.
 Proof. by case sF=> Fobj; case. Qed.
 
-Lemma bisfc_norm : forall G, G \subset 'N('e_sF(G)).
+Lemma bisfc_norm : forall gT (G:{group gT}), G \subset 'N('e_sF(G)).
 Proof.
-move=> G; apply/subsetP=> x Gx; rewrite inE -{2}(conjGid Gx) -{2}(setIid G).
-rewrite -(setIidPr (bisfc_clos G)) -!morphim_conj.
-pose conjgx := (autm (Aut_aut (@injm_conj _ G _) 
-               (norm_conj_dom (valP (insigd (group1 _) x))))).
-rewrite -!(@eq_morphim _ _ _ [morphism of conjgx] (conjgm_morphism _ x)) =>/=;
-rewrite ?bisfc_clos ?bisfc_aresp // => y; apply: (conj_autE Gx).
+move=> gT G; apply/subsetP=> x Gx; rewrite inE -{2}(conjGid Gx) -{2}(setIid G).
+by rewrite -(setIidPr (bisfc_clos G)) -!morphim_conj bisfc_aresp ?injm_conj.
 Qed.
 
-Lemma bisfc_normal : forall G, 'e_sF(G) <| G.
-Proof. by move=> G; apply/andP; rewrite bisfc_clos bisfc_norm. Qed.
-
-Lemma bisfc_char : forall G, 'e_sF(G) \char G.
+(* independent from the group preservation requirement*)
+Lemma bisfc_gen_norm : forall gT (G:{group gT}),
+  <<'e_sF(G)>> \subset 'N('e_sF(G)).
 Proof.
-move=> G; apply/andP; split => //; first by apply: bisfc_clos.
+move=> gT G; apply/normsP=> x Hx; move/normsP: (bisfc_norm G); apply.
+by move: ((subsetP (genS (bisfc_clos G))) _ Hx); rewrite genGid.
+Qed.
+
+Lemma bisfc_normal : forall gT (G : {group gT}), 'e_sF(G) <| G.
+Proof. by move=> gT G; apply/andP; rewrite bisfc_clos bisfc_norm. Qed.
+
+Lemma bisfc_char : forall gT (G:{group gT}), 'e_sF(G) \char G.
+Proof.
+move=> gT G; apply/andP; split => //; first by apply: bisfc_clos.
 apply/forallP=> f; apply/implyP=> Af; rewrite -{2}(autm_dom Af).
-rewrite  -(morphimEsub (autm_morphism Af)) ?bisfc_clos //.
-by apply: bisfc_aresp.
+by rewrite -(morphimEsub (autm_morphism Af)) ?bisfc_clos ?bisfc_aresp ?injm_autm.
+Qed.
+
+Lemma bisfc_asresp_sub :
+  forall gT hT (H G:{group gT})  (f : {morphism G >-> hT}),
+  'injm f -> H \subset G -> f @* ('e_sF(H)) \subset 'e_sF(f @* H).
+Proof.
+move=> gT hT H G f injf sHG; apply/eqP; rewrite -(setIidPr (bisfc_clos H)).
+by rewrite-{3}(setIid H) -!(morphim_restrm sHG) bisfc_aresp ?injm_restrm //=.
+Qed.
+
+Lemma bisfc_asresp : forall gT hT (H G : {group gT}) (f : {morphism G >-> hT}),
+  'injm f -> H \subset G -> f @* ('e_sF(H)) = 'e_sF(f @* H).
+Proof.
+move=> gT hT H G f injf sHG; apply/eqP; rewrite eqEsubset bisfc_asresp_sub //=.
+rewrite -{2}(morphim_invm injf sHG) -[f @* sF _ _](morphpre_invm injf).
+have sFtr := subset_trans (bisfc_clos _).
+by rewrite -sub_morphim_pre (bisfc_asresp_sub, sFtr) ?morphimS ?injm_invm.
+Qed.
+
+Lemma bisfc_nker_resp : forall gT hT (H G : {group gT})
+  (f : {morphism G >-> hT}),
+  'ker f = 1 -> H \subset G -> f @* ('e_sF(H)) = 'e_sF(f @* H).
+Proof. by move=> gT hT H G f; move/trivgP =>/=; apply:bisfc_asresp. Qed.
+
+Lemma bisfc_isom : forall gT hT (H G : {group gT}) (R : {group hT}) 
+                                   (f : {morphism G >-> hT}),
+  H \subset G -> isom H R f -> isom ('e_sF(H)) ('e_sF(R)) f.
+Proof.
+move=> gT rT H G R f; case/(restrmP f)=> g [_ _ ->]; case/isomP=> injf <-.
+rewrite /isom -(bisfc_asresp injf) // -morphimEsub ?morphimDG ?morphim1 //. 
+by rewrite subDset subsetU // bisfc_clos orbT.
+Qed.
+
+Lemma bisfc_isog : forall gT hT (G : {group gT}) (R : {group hT}),
+  G \isog R -> 'e_sF(G) \isog 'e_sF(R).
+Proof.
+move=> gT rT D R; case/isogP=> f *.
+apply: (isom_isog f) => //; first by apply: bisfc_clos.
+apply: bisfc_isom => //; exact/isomP.
 Qed.
 
 End BaseIdentitySubfunctorProps.
@@ -140,35 +209,25 @@ Module IdSubFunctor.
 
 Implicit Types gT: finGroupType.
 
-Structure class : Type := Class { 
-  Fobj :> obmap ;
-   _ : BaseIdSubFunctor.mixin Fobj;
-   _ : resp Fobj}.
+Structure isFcClass : Type := Class { 
+  FbisFc :> bisFc ;
+   _ : resp FbisFc}.
 
-Coercion mixin_of F := let: Class _ m _ := F return 
-  BaseIdSubFunctor.mixin F in m.
-
-Lemma isfc_resp (sF: class): resp sF.
+Lemma isfc_resp (sF: isFcClass): resp sF.
 Proof. by case => F FM. Qed.
 
 End IdSubFunctor.
 
-Notation isFc := IdSubFunctor.class.
+Notation isFc := IdSubFunctor.isFcClass.
 Notation IsFc := IdSubFunctor.Class.
 
-Definition mkIsFc F Fsub (Fresp:resp F) := 
-  IsFc (BisFMixin Fsub (aresp_of_resp Fresp)) Fresp.
+Definition mkIsFc F Fgrp Fsub (Fresp:resp F) := 
+  @IsFc (mkBaseisFc Fgrp Fsub (aresp_of_resp Fresp)) Fresp.
 
 Notation "[ 'isFc' 'of' F ]" :=
-  (match [is F : _ -> _ <: bisFc] as s return {type of BisFc for s} -> _ with
-    | IsFc _ m r => fun k => k m r end
+  (match [is F : _ -> _ <: isFc] as s return {type of IsFc for s} -> _ with
+    | IsFc _ m => fun k => k m end
   (@IsFc F)) (at level 0, only parsing) : form_scope.
-
-Coercion isFc_bisFc (sF : isFc) := 
-  BisFc (BisFMixin (BaseIdSubFunctor.subset_of sF)
-  (aresp_of_resp (IdSubFunctor.isfc_resp sF))).
-
-Canonical Structure isFc_bisFc.
 
 Section IdentitySubFunctorProps.
 
@@ -188,29 +247,16 @@ Qed.
 
 Lemma injm_sfunctor : forall gT hT (G D : {group gT}) (f : {morphism D >-> hT}),
   'injm f -> G \subset D -> f @* ('e_sF(G)) = 'e_sF(f @* G).
-Proof.
-move=> gT hT G D f injf sGD; apply/eqP; rewrite eqEsubset morphim_sfunctor //=.
-rewrite -{2}(morphim_invm injf sGD) -[f @* sF _ _](morphpre_invm injf).
-have sFtr := subset_trans (bisfc_clos _ _).
-by rewrite -sub_morphim_pre (morphim_sfunctor, sFtr) ?morphimS.
-Qed.
+Proof. exact: bisfc_asresp. Qed.
 
 Lemma isom_sfunctor : forall gT hT (D G : {group gT}) (R : {group hT}) 
                                    (f : {morphism D >-> hT}),
   G \subset D -> isom G R f -> isom ('e_sF(G)) ('e_sF(R)) f.
-Proof.
-move=> gT rT D G R f; case/(restrmP f)=> g [_ _ ->]; case/isomP=> injf <-.
-rewrite /isom -(injm_sfunctor injf) // -morphimEsub ?morphimDG ?morphim1 //. 
-by rewrite subDset subsetU // bisfc_clos orbT.
-Qed.
+Proof. move=> gT hT D G; exact:bisfc_isom. Qed.
 
 Lemma isog_sfunctor : forall gT hT (D : {group gT}) (R : {group hT}),
   D \isog R -> 'e_sF(D) \isog 'e_sF(R).
-Proof.
-move=> gT rT D R; case/isogP=> f *.
-apply: (isom_isog f) => //; first by apply: bisfc_clos.
-apply: isom_sfunctor => //; exact/isomP.
-Qed.
+Proof. exact: bisfc_isog. Qed.
 
 End IdentitySubFunctorProps.
 
@@ -222,16 +268,11 @@ Module DecrIdSubFunctor.
 Implicit Types gT: finGroupType.
 
 Structure class : Type := Class { 
-  Fobj :> obmap;
-   _ : BaseIdSubFunctor.mixin Fobj;
-   _ : resp Fobj;
-   _ : decr Fobj}.
-
-Coercion mixin_of F := let: Class _ m _ _ := F return 
-  BaseIdSubFunctor.mixin F in m.
+  FisFc :> isFc;
+   _ : decr FisFc}.
 
 Lemma disfc_resp (sF: class): resp sF.
-Proof. by case => F. Qed.
+Proof. by move=> sF; apply:isfc_resp. Qed.
 
 Lemma disfc_decr (sF : class): decr sF.
 Proof. by case => F. Qed.
@@ -241,22 +282,133 @@ End DecrIdSubFunctor.
 Notation disFc := DecrIdSubFunctor.class.
 Notation DisFc := DecrIdSubFunctor.Class.
 
-Definition mkdIsFc F Fsub (Fresp:resp F) (Fdecr : decr F) := 
-  DisFc (BisFMixin Fsub (aresp_of_resp Fresp)) Fresp Fdecr.
+Definition mkdIsFc F Fgrp Fsub (Fresp:resp F) (Fdecr : decr F) := 
+  @DisFc (mkIsFc Fgrp Fsub Fresp) Fdecr.
 
 Notation "[ 'disFc' 'of' F ]" :=
-  (match [is F : _ -> _ <: bisFc] as s return {type of BisFc for s} -> _ with
-    | DisFc _ m r d => fun k => k m r d end
+  (match [is F : _ -> _ <: disFc] as s return {type of DisFc for s} -> _ with
+    | DisFc _ d => fun k => k  d end
   (@DisFc F)) (at level 0, only parsing) : form_scope.
-
-Coercion disFc_isFc (sF : disFc) := 
-  IsFc sF (DecrIdSubFunctor.disfc_resp sF).
-
-Canonical Structure disFc_isFc.
 
 Section DecreasingIdentitySubFunctorProps.
 
+Implicit Types gT hT:finGroupType.
+
+Section DecreasingIdBaseProps.
+
+Variable sF: disFc.
+
+Lemma disfc_resp : resp sF.
+Proof. exact:IdSubFunctor.isfc_resp. Qed.
+
+Lemma disfc_decr : decr sF.
+Proof. exact:DecrIdSubFunctor.disfc_decr. Qed.
+
+Lemma disfc_imsetI : forall gT (G H:{group gT}),
+  'e_sF(G) :&: H = 'e_sF (G) :&: 'e_sF (G :&: H).
+Proof.
+move=>gT G H; rewrite -{1}(setIidPr (bisfc_clos sF G)) [G :&: _]setIC -setIA.
+move/setIidPr: (disfc_decr (subsetIl G H)) <-.
+by rewrite setIC -setIA (setIidPr (bisfc_clos sF (G:&: H))).
+Qed.
+
+Lemma disfc_morphim : dresp sF.
+Proof.
+move=> gT hT G D f.
+rewrite -morphimIdom -(setIidPl (bisfc_clos sF G)) setIC -setIA [G :&: _]setIC.
+apply: (subset_trans (morphimS f (disfc_decr (subsetIr D G)))).
+apply:(subset_trans (morphim_sfunctor sF _ _ )); last by rewrite morphimIdom.
+exact:subsetIl.
+Qed.
+
+End DecreasingIdBaseProps.
+
+Section DecreasingIdentitySFComp.
+
+Variable sF: disFc.
+Variable sF2 : isFc.
+
+(* independent from the group preservation requirement *)
+Lemma disfc_comp_clos : forall gT (G:{group gT}),
+  (appmod sF sF2) gT G \subset G.
+Proof.
+move=> gT G; rewrite /appmod; have nFD := (bisfc_norm sF2 G).
+rewrite sub_morphpre_im ?(bisfc_clos sF) //=.
+  by rewrite ker_coset_prim subIset // gen_subG (bisfc_clos sF2 G).
+by apply: subset_trans (bisfc_clos sF _) _; apply: morphimS.
+Qed.
+
+Lemma disfc_comp_idsubfunc : forall gT (G : {group gT}) (B : {set gT}),
+  (coset B @*^-1  sF _ (G / B)) / B  = sF _ (G / B).
+Proof.
+move=> gT A B; apply: morphpreK; apply: subset_trans (bisfc_clos sF _) _.
+by rewrite /= /quotient -morphimIdom  morphimS ?subsetIl.
+Qed.
+
+Lemma disfc_comp_resp : resp (appmod sF sF2).
+move=> gT rT G f; have nF := bisfc_norm sF2.
+have kF := ker_coset (Group (bisfc_groupset sF2 _)); simpl in kF.
+have sDF: G \subset 'dom (coset (sF2 _ G)) by rewrite ?nF.
+have sDFf: G \subset 'dom (coset (sF2 _ (f @* G)) \o f).
+  by rewrite -sub_morphim_pre ?subsetIl // nF.
+pose K := 'ker (restrm sDFf (coset (sF2 _ (f @* G)) \o f)).
+have sFK: 'ker (restrm sDF (coset (sF2 _ G))) \subset K.
+  rewrite /K  !ker_restrm ker_comp /= subsetI subsetIl /=.
+  by rewrite -sub_morphim_pre ?subsetIl // morphimIdom !kF ?morphim_sfunctor.
+have sOF := bisfc_clos sF (G / sF2 _ G); have sGG: G \subset G by [].
+rewrite -sub_morphim_pre -?quotientE; last first.
+  by apply: subset_trans (nF _ _); rewrite morphimS ?disfc_comp_clos.
+suffices im_fact: forall H : {group gT}, sF2 _ G \subset H -> H \subset G ->
+  factm sGG sFK @* (H / sF2 _ G) = f @* H / sF2 _ (f @* G).
+- rewrite -2?im_fact ?disfc_comp_clos ?bisfc_clos //.
+  by rewrite disfc_comp_idsubfunc morphim_sfunctor /= ?morphim_restrm ?setIid //.
+  by rewrite -{1}kF morphpreS ?sub1G.
+move=> H sFH sHG; rewrite -(morphimIdom _ (H / _)) /= {2}morphim_restrm setIid.
+rewrite -morphimIG ?kF // -(morphim_restrm sDF) morphim_factm morphim_restrm.
+by rewrite morphim_comp -quotientE morphimIdom.
+Qed.
+
+End DecreasingIdentitySFComp.
+
+(*The structure for this one is un-inferrable, see morphim for phantom creation*)
+Definition disfc_comp_isfc (sF:disFc) (sF2:isFc) :=
+  @mkIsFc (appmod sF sF2)
+  (fun gT G => groupP [group of ((appmod sF sF2) _ G)])
+  (disfc_comp_clos sF sF2)
+  (disfc_comp_resp sF sF2).
+
+(* TOFIX *)
+
+Variables sF sF3:disFc.
+
+Lemma disfc_comp_dresp : dresp (disfc_comp_isfc sF sF3).
+Proof.
+move=> gT rT G D f /=; have nF := bisfc_norm sF3.
+have kF := ker_coset (Group (bisfc_groupset sF3 _)); simpl in kF.
+have sDF: D :&: G \subset 'dom (coset (sF3 _ G)) by rewrite setIC subIset ?nF.
+have sDFf: D :&: G \subset 'dom (coset (sF3 _ (f @* G)) \o f).
+  by rewrite -sub_morphim_pre ?subsetIl // morphimIdom nF.
+pose K := 'ker (restrm sDFf (coset (sF3 _ (f @* G)) \o f)).
+have sFK: 'ker (restrm sDF (coset (sF3 _ G))) \subset K.
+  rewrite /K  !ker_restrm ker_comp /= subsetI subsetIl /= -setIA.
+  by rewrite -sub_morphim_pre ?subsetIl // morphimIdom !kF (setIidPr _) ?disfc_morphim ?bisfc_clos.
+have sOF := bisfc_clos sF (G / sF3 _ G); have sGG: D :&: G \subset D :&: G by [].
+rewrite -sub_morphim_pre -?quotientE; last first.
+  by apply: subset_trans (nF _ _); rewrite morphimS ?disfc_comp_clos.
+suffices im_fact: forall H : {group gT}, sF3 _ G \subset H -> H \subset G ->
+  factm sGG sFK @* (H / sF3 _ G) = f @* H / sF3 _ (f @* G).
+- rewrite -2?im_fact ?disfc_comp_clos ?bisfc_clos //.
+  by rewrite disfc_comp_idsubfunc disfc_morphim /=.
+  by rewrite -{1}kF morphpreS ?sub1G.
+move=> H sFH sHG; rewrite -(morphimIdom _ (H / _)) /= {2}morphim_restrm setIid.
+rewrite -morphimIG ?kF // -(morphim_restrm sDF) morphim_factm morphim_restrm.
+by rewrite morphim_comp -quotientE -setIA morphimIdom (setIidPr _).
+Qed.
+
 End DecreasingIdentitySubFunctorProps.
+
+Definition disfc_comp_disfc (sF sF3:disFc)  :=
+  DisFc (decr_of_dresp (disfc_comp_dresp sF sF3)).
 
 Section IdentitySubfunctorsExamples.
 
@@ -266,15 +418,18 @@ Lemma id_resp : resp (fun _ S => S).
 Proof. by []. Qed.
 
 Canonical Structure id_subfunc :=
-  mkIsFc (fun gT G => subxx _)
-         id_resp.
+  mkIsFc (fun gT G => groupP G%G)
+  (fun gT G => subxx _)
+  id_resp.
 
 Lemma triv_resp : resp (fun _ S => 1).
 Proof. by move=> gT hT H f; rewrite morphim1. Qed.
 
 Canonical Structure triv_subfunc :=
-  @mkIsFc (fun _ S => 1) sub1G
-        triv_resp.
+  @mkIsFc (fun _ S => 1)
+  (fun _ G => groupP 1%G)
+  sub1G
+  triv_resp.
 
 Require Import center.
 
@@ -285,9 +440,15 @@ rewrite subsetI subsetIl /=; apply:(subset_trans (subsetIr (phi @* H) _) ).
 exact:morphim_cent.
 Qed.
 
+Lemma center_decr : decr (fun _ S => 'Z(S)).
+Proof.
+move=> gT H G sHG; rewrite setIC /center setIA (setIidPl sHG) setIS //.
+by rewrite (centsS sHG).
+Qed.
+
 Canonical Structure center_id_subfunc :=
-  mkIsFc (fun gT G => @center_sub gT G)
-         center_resp.
+  mkdIsFc (fun gT G => groupP 'Z(G)%G) (fun gT G => @center_sub gT G)
+         center_resp center_decr.
 
 Require Import maximal.
 
@@ -295,7 +456,7 @@ Lemma Frattini_resp : resp (Frattini).
 Proof. exact: gfunc_Phi. Qed.
 
 Canonical Structure Frattini_subfunc :=
-  mkIsFc (fun gT => @Phi_sub gT)
+  mkIsFc (fun gT G => groupP 'Phi(G)%G) (fun gT => @Phi_sub gT)
          Frattini_resp.
 
 Require Import nilpotent.
@@ -315,7 +476,8 @@ by rewrite commgSS ?IH.
 Qed.
 
 Canonical Structure der_id_subfunctor (n:nat) :=
-  mkIsFc (der_clos n)
+  mkIsFc (fun gT (G:{group gT}) => der_group_set G n)
+         (der_clos n)
          (der_resp n).
 
 Lemma lcn_resp : forall n,
@@ -326,8 +488,9 @@ by rewrite !lcnSn morphimR ?lcn_sub0 // commSg ?IH.
 Qed.
 
 Canonical Structure lcn_id_subfunctor (n:nat) :=
-  mkIsFc (fun gT G => (lcn_sub0 G n))
-         (lcn_resp n).
+  mkIsFc (fun gT (G :{group gT}) => lcn_group_set G n)
+  (fun gT G => (lcn_sub0 G n))
+  (lcn_resp n).
 
 Require Import pgroups.
 
@@ -339,8 +502,9 @@ Lemma Ohm_resp : forall i,
 Proof. move=> i G f; exact: gfunc_Ohm. Qed.
 
 Canonical Structure Ohm_id_subfunctor (i:nat) :=
-  mkIsFc (Ohm_sub i)
-         (Ohm_resp i).
+  mkIsFc (fun gT (G:{group gT}) => groupP 'Ohm_i(G)%G)
+  (Ohm_sub i)
+  (Ohm_resp i).
 
 Lemma Mho_sub : forall i gT (G:{group gT}), 'Mho^i(G) \subset G.
 Proof. move=> i; exact: Mho_sub. Qed.
@@ -350,8 +514,9 @@ Lemma Mho_resp : forall i,
 Proof. move=> i G f; exact: gfunc_Mho. Qed.
 
 Canonical Structure Mho_id_subfunctor (i:nat) :=
-  mkIsFc (Mho_sub i)
-         (Mho_resp i).
+  mkIsFc (fun gT (G:{group gT}) => groupP 'Mho^i(G)%G)
+  (Mho_sub i)
+  (Mho_resp i).
 
 End IdentitySubfunctorsExamples.
 
