bccbe219f5ab58ccfe991e4bff546c457eaf568a
gareuselesinge <gareuselesinge@305884b8-be16-0410-aa53-a3be2363cef1>
2009-11-25 15:14:10 +0000

    shorter proof of 6.3(a)
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/coqfinitgroup/trunk@1591 305884b8-be16-0410-aa53-a3be2363cef1

--- BGsection6.v
+++ BGsection6.v
@@ -19,56 +19,35 @@ Lemma six3a : forall G H K : {group gT},
    [~: H, K] = H /\ 'C_H(K) \subset H^`(1).
 Proof.
 move=> G H K solG hallH; case/sdprodP=> _ defG nHK tiHK sHG'.
-set Hstar := [~: H, K].
-case/andP: (hallH) => sHG.
+case/andP: hallH => sHG; case/andP: (der_normal H 0) =>  sHH' nH'H.
 rewrite -divgS // -defG TI_cardMg // mulKn // => coHK.
-pose Gbar := G / Hstar; pose Hbar := H / Hstar; pose Kbar := K / Hstar.
-have tiHbKb : Hbar :&: Kbar = 1
-  by rewrite -quotientGI // ?tiHK ?quotient1 // commg_subl. 
-have GHxK : Gbar = Hbar \x Kbar.
-  rewrite dprodE /Gbar -?defG ?tiHbKb //; first by rewrite quotientMl // normsRl.
-  by rewrite quotient_cents2 //; [ rewrite normsRl | rewrite normsRr ].
-have sHbH'K' : Hbar \subset Hbar^`(1) * Kbar^`(1).
-  have p1 : G \subset 'N(Hstar).   
-    by apply: (subset_trans _ (commg_norm _ _)); rewrite norm_mulgenEr ?defG.
-  have sHbGb' : Hbar \subset Gbar^`(1).
-    by rewrite /Hbar derg1 /Gbar -quotientR // quotientS.
-  apply: (subset_trans sHbGb').
-  rewrite !derg1 -![[~: _, _]]lcn1 /Gbar -defG -lcn_mul ?quotient_cents2r //.
-  by rewrite !lcn1 !derg1 -quotientMl ?normsRl.
-have {tiHbKb} tiHbKb' : Hbar :&: Kbar^`(1) = 1.
-  have s1G : forall G : {group coset_of Hstar}, 1%G \subset G. (* ??? *)
-    by move=>*;apply/subsetP=>x; case/set1P=>->; apply: group1.
-  apply/eqP; rewrite eqEsubset subsetI !s1G !andbT.  
-  have aux : Hbar :&: Kbar^`(1) \subset Hbar :&: Kbar
-    by rewrite setIS // der_sub.
-  by apply: (subset_trans aux); rewrite tiHbKb.
-have nH'K' : Kbar^`(1) \subset 'N(Hbar^`(1)).
+set R := [~: H, K]; have sRH: R \subset H by rewrite commg_subl.
+have tiHbKb : H / R :&: K / R = 1 by rewrite -quotientGI ?tiHK ?quotient1.
+have sHbH'K' : H / R \subset (H / R)^`(1) * (K / R)^`(1).
+  have nRG: G \subset 'N(R) by rewrite -defG -norm_mulgenEr ?commg_norm.
+  rewrite -(lcn_mul 1) ?quotient_cents2r //= -quotientMl ?normsRl //= -/R.
+  by rewrite defG -['L_1(_)]quotientR // quotientS.
+have {tiHbKb} tiHbKb' : H / R :&: (K / R)^`(1) = 1.
+  by apply/trivgP; rewrite /= -tiHbKb setIS // der_sub.
+have nH'K' : (K / R)^`(1) \subset 'N((H / R)^`(1)).
   by rewrite normsR // (subset_trans (der_sub _ _)) // quotient_norms.
-have {nH'K' tiHbKb' sHbH'K'} Hb'eqHb : Hbar^`(1) = Hbar. 
-  rewrite -(@mul1g _ Hbar^`(1)) -tiHbKb' group_modr /= -/Hbar ?der_sub0 //.
+have {nH'K' tiHbKb'} HR'HR : (H / R)^`(1) = H / R. 
+  rewrite -[_^`(_)]mul1g -tiHbKb' group_modr /= ?der_sub0 //.
   by apply/setIidPl; rewrite -norm_mulgenEl // mulgenC norm_mulgenEr.
-have {Hb'eqHb} HbT : Hbar = 1.
-  have solHb': solvable Hbar^`(1). 
-    by rewrite Hb'eqHb quotient_sol // (solvableS _ solG).
-  apply/eqP; apply: (implyP (forallP solHb' _)); rewrite /= -/Hbar.
-  by rewrite -{1}Hb'eqHb subsetI subxx.
-have {HbT GHxK Gbar Kbar Hbar} fstHalf: [~: H, K ] = H. 
-  apply/eqP; rewrite eqEsubset /Hstar !commg_subl nHK /=.
-  by rewrite -quotient_sub1 -/Hbar ?HbT ?commg_norml.
-pose H' := H^`(1).
-suff E: 'C_H(K) / H' = 1. 
-  rewrite -quotient_sub1 ?E // (subset_trans _ (commg_norml _ _)) //. 
-  by rewrite (subset_trans _ (subsetIl _ 'C(K))) //.
-have abelHH' : abelian (H / H') by apply: der_abelian.
-have copH' : coprime #|H / H'| #|K / H'| by apply: coprime_morph.
-have sKH'NormHH' : K / H' \subset 'N(H / H') by rewrite quotient_norms.
-have nH'K : K \subset 'N(H') by apply: char_norm_trans nHK; apply: der_char.
-have: H' <| H by apply: der_normal.
-case/andP=> sHH' nH'H.
-rewrite coprime_quotient_cent //= -/H' //.
-  by rewrite -fstHalf quotientR //= -/H' coprime_abel_cent_TI.
-exact: solvableS solG.
+have {HR'HR} HbT : (H / R) = 1.
+  have solHb': solvable (H / R)^`(1).
+    by rewrite HR'HR quotient_sol //; apply: solvableS _ solG.
+  by rewrite (eqP (implyP (forallP solHb' _) _)) //= -{1}HR'HR subsetI subxx.
+have {HbT sHbH'K' sRH} fstHalf: [~: H, K ] = H. 
+  apply/eqP; rewrite eqEsubset -/R //= !commg_subl nHK /=.
+  by rewrite -quotient_sub1 ?HbT ?commg_norml.
+suff {R} E: 'C_H(K) / H^`(1) = 1 by rewrite -quotient_sub1 ?E // subIset // nH'H.
+have abelHH' : abelian (H / H^`(1)) by apply: der_abelian.
+have copH' : coprime #|H / H^`(1)| #|K / H^`(1)| by apply: coprime_morph.
+have nKH'HH' : K / H^`(1) \subset 'N(H / H^`(1)) by rewrite quotient_norms.
+have nH'K : K \subset 'N(H^`(1)) by apply: char_norm_trans nHK; apply: der_char.
+rewrite coprime_quotient_cent //=; last exact: solvableS solG.
+by rewrite -{4}fstHalf quotientR //= coprime_abel_cent_TI.
 Qed.
 
 End Test.
