3aa92547da5c46a9e481651cd088b48d742f078b
ccohen <ccohen@305884b8-be16-0410-aa53-a3be2363cef1>
2009-12-24 12:35:47 +0000

    Minor corrections in quotient
    Including pack -> clone
    
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/coqfinitgroup/trunk@1619 305884b8-be16-0410-aa53-a3be2363cef1

--- quotient.v
+++ quotient.v
@@ -1,18 +1,15 @@
-Require Import ssreflect ssrfun ssrbool eqtype ssrnat div seq choice fintype finfun tuple.
-Require Import bigops ssralg.
-
+(* -*- coding : utf-8 -*- *)
+Require Import ssreflect ssrfun ssrbool eqtype ssrnat.
 
 Set Implicit Arguments.
 Unset Strict Implicit.
 Import Prenex Implicits.
 
 Reserved Notation "\pi_ T" (at level 0, format "\pi_ T").
-Reserved Notation "x == y 'mod' T"
-  (at level 70, y at next level, no associativity, format "x  ==  y  'mod'  T").
-Reserved Notation "\equiv_ T x y" (at level 2, format "\equiv_ T  x  y").
+Reserved Notation "x == y 'mod' T" (at level 70, y at next level, 
+  no associativity, format "x  ==  y  'mod'  T").
 Reserved Notation "\compat1_ T" (at level 0, format "\compat1_ T").
 Reserved Notation "\compat2_ T" (at level 0, format "\compat2_ T").
-
 
 Section Quotient.
 
@@ -48,20 +45,9 @@ Proof. by move=> K PK u; rewrite (quot_sortquot_Sub u); apply:PK. Qed.
 
 Canonical Structure quot_subType := SubType _ _ _ reprP reprK.
 Definition quot_eqMixin := Eval hnf in [eqMixin of qT by <:].
-Canonical Structure quot_eqType := Eval hnf in EqType qT quot_eqMixin.
+Canonical Structure quot_eqType := Eval hnf in EqType _ quot_eqMixin.
 
-Definition repack_quot qT :=
-  let: QuotType _ repr' pi' can := qT
-  return {type of @QuotType for qT } -> _ in
-  fun k => k repr' pi' can.
-
-Notation "[ 'quotType' 'of' T ]" :=
-   (repack_quot (fun r p can => @QuotType T r p can))
- (at level 0, format "[ 'quotType'  'of'  T ]") : form_scope.
-
-Lemma Sub_pi : forall x Px, @Sub T _ _ x Px = pi qT x.
-Proof. by move=> x Px; rewrite /= /quot_Sub. Qed.
-
+Definition clone_quot (Q:Type) qT of phant_id (quot_sort qT) Q := qT.
 
 Notation "\pi_ T" := (@pi_of (Phant T)).
 Notation "x == y 'mod' T" := (\pi_T x == \pi_T y).
@@ -80,10 +66,8 @@ End Quotient.
 Notation "\pi_ T" := (@pi_of _ _ (Phant T)).
 Notation "x == y 'mod' T" := (\pi_T x == \pi_T y).
 
-
-Notation "[ 'quotType' 'of' T ]" :=
-   (repack_quot (fun r p can => @QuotType T r p can))
- (at level 0, format "[ 'quotType'  'of'  T ]") : form_scope.
+Notation "[ 'quotType' 'of' Q ]" := (@clone_quot _ Q _ id)
+ (at level 0, format "[ 'quotType'  'of'  Q ]") : form_scope.
 
 
 Section Congruence.
@@ -105,7 +89,9 @@ Qed.
 Lemma compat1Epi : forall (op:T -> T),
   (forall x y,  (x == y mod qT) -> (op x == op y mod qT))
   -> compat1 (\pi_qT \o op).
-Proof. move=> op Hyp. apply:compat1E=> x y exy. apply/eqP. exact: Hyp. Qed.
+Proof. 
+move=> op Hyp. apply:compat1E=> x y exy. apply/eqP. exact: Hyp. 
+Qed.
 
 
 CoInductive compat2 S (op : T -> T -> S) : Type :=
@@ -127,8 +113,8 @@ Lemma compat2Epi : forall (op : T -> T -> T),
   (forall x x' y y',  x == x' mod qT -> y == y' mod qT
     -> op x y == op x' y' mod qT ) -> compat2 (fun x y => \pi_qT (op x y)).
 Proof.
-move=> op Hyp. apply:compat2E=> x y x' y' exy exy'.
-apply/eqP. exact: Hyp.
+move=> op Hyp; apply:compat2E=> x y x' y' exy exy'. 
+apply/eqP; exact: Hyp. 
 Qed.
 
 Lemma qT_op1E : forall S op cop x,
@@ -145,7 +131,7 @@ Definition qTE := (qT_op1E, qT_op2E).
 
 Lemma quotW : forall P, (forall y:T, P (\pi_qT y)) -> forall x:qT, P x.
 Proof.
-move=> P Py x. set y := repr x. have -> : x = pi qT y; last exact: Py.
+move=> P Py x; set y := repr x; have -> : x = pi qT y; last exact: Py.
 by rewrite /y /=; rewrite pi_repr.
 Qed.
 
@@ -153,47 +139,14 @@ Lemma quotP : forall P,
   (forall y:T, (repr (\pi_qT y) = y)
   -> P (\pi_qT y)) -> forall x:qT, P x.
 Proof.
-move=> P Py x. set y := repr x. have -> : x = pi qT y.
+move=> P Py x. set y := repr x; have -> : x = pi qT y.
    by rewrite /y /= pi_repr.
 by apply: Py; rewrite /y /= /pi_of pi_repr.
 Qed.
 
-
 End Congruence.
 Notation "\compat1_ T" := (compat1_of (Phant T)).
 Notation "\compat2_ T" := (compat2_of (Phant T)).
-
-
-Section cartesianSquare.
-Variable R : choiceType.
-
-Definition cartesianSquare := (R * R)%type.
-
-Definition csquare_to_tuple (x : cartesianSquare) := [tuple x.1; x.2].
-Definition tuple_to_csquare (t: 2.-tuple R) :=  ([tnth t 0], [tnth t 1]).
-
-Lemma csquare_tupleK : cancel csquare_to_tuple tuple_to_csquare.
-Proof. by elim. Qed.
-
-Definition csquareEqMixin := CanEqMixin csquare_tupleK.
-(* Canonical Structure csquareEqType := Eval hnf in EqType csquareEqMixin. *)
-Canonical Structure csquareEqType := Eval hnf in [eqType of cartesianSquare].
-Definition csquareChoiceMixin := CanChoiceMixin csquare_tupleK.
-Canonical Structure csquareChoiceType :=
-  Eval hnf in ChoiceType cartesianSquare csquareChoiceMixin.
-
-Identity Coercion csquareToProd : cartesianSquare >-> prod.
-
-Definition csquare_of of phant R := cartesianSquare.
-Identity Coercion type_csquare_of : csquare_of >-> cartesianSquare.
-
-Notation "{ 'csquare' T }" := (csquare_of (Phant T)) (at level 0, format "{ 'csquare'  T }").
-Canonical Structure csquareEqType' := Eval hnf in [eqType of {csquare R}].
-Canonical Structure csquareChoiceType' := Eval hnf in [choiceType of {csquare R}].
-
-End cartesianSquare.
-Notation "{ 'csquare' T }" := (csquare_of (Phant T)) (at level 0, format "{ 'csquare'  T }").
-
 
 
 
