682d1dd700c6ff9d9e4666750f66007dde5dd64a
gareuselesinge <gareuselesinge@305884b8-be16-0410-aa53-a3be2363cef1>
2010-02-09 17:35:13 +0000

    some more work on section 5
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/coqfinitgroup/trunk@1694 305884b8-be16-0410-aa53-a3be2363cef1

--- BGsection5.v
+++ BGsection5.v
@@ -108,8 +108,10 @@ case: (ltnP (logn p #|Bs|) 3); move=> cardBs.
     rewrite card_quotient ?normsI ?normG ?(subset_trans _ (cent_norm E)) //.
     by rewrite logn_quotient_cent_abelem ?cardE ?(subset_trans sBR).
   have cardCBE : 2 <= logn p #|'C_B(E)|.
-    move: cardBq'; rewrite card_quotient ?normsI ?normG ?(subset_trans nEB (norms_cent _)) //. 
-    rewrite -divgS ?subIset ?subxx //= logn_div ?cardB ?cardSg ?subIset ?subxx //.
+    move: cardBq'; rewrite card_quotient; last first. 
+      by rewrite normsI ?normG ?(subset_trans nEB (norms_cent _)). 
+    rewrite -divgS ?subIset ?subxx //=. 
+    rewrite logn_div ?cardB ?cardSg ?subIset ?subxx //.
     by case: (logn _ _)=> //; case.
   have defE' : 'C_B(E) :=: E.
     apply/eqP; rewrite eqEcard sCE (logn_injcard pE) // 1?cardE ?cardCBE //=.
@@ -121,7 +123,68 @@ case: (ltnP (logn p #|Bs|) 3); move=> cardBs.
     apply/eqP; rewrite eqEsubset subIset ?subxx // subsetI subxx /=.
     by rewrite sub_abelian_cent // proper_sub.
   by move/eqP: abs2; rewrite eqEproper; case/andP=> _; case/negP; rewrite defE'.
-have := centralizer_scn_pgroup_eq.
+have p44 := centralizer_scn_pgroup_eq.
+admit.
+Qed.
+
+Lemma five_2 : forall R E p,
+  odd #|R| -> p.-group(R) -> 2 < 'r(R) -> E \in 'E*_p(R) -> 'r(E) = 2 ->
+  let T := 'C_R('Ohm_1('Z_2(R))) in
+  [/\ ~~ (E \subset T),  #| 'Ohm_1('Z(R)) | = p, 'Ohm_1('Z_2(R))%G \in 'E_p^2(R),
+      T \char R & #| R : T | = p ].
+Proof.
+move=> R E p oddp pR rankR pmaxE; case/pmaxElemP: (pmaxE).
+case/pElemP=> sER abelemE maxE rankE T.
+have ntR : (R != 1)%G by rewrite -rank_gt0 (ltn_trans _ rankR).
+have [p_pr pdvR [r oRpr]] := pgroup_pdiv pR ntR.
+pose Z := 'Ohm_1('Z(R)); pose W := 'Ohm_1('Z_2(R)).
+have defEZ : E <*> Z :=: E * Z.
+  have ? : R \subset 'N('Z(R)) by rewrite ?(char_norm _) ?center_char.
+  by rewrite /= norm_mulgenEl ?(char_norm_trans (Ohm_char 1 'Z(R))) ?(subset_trans sER) //=.
+have pZ : p.-group 'Z(R) by rewrite (pgroupS (center_sub _)).
+have ? := abelian_center R.
+have abelemEZ : p.-abelem (E <*> Z).
+  apply/(abelemP p_pr); split.
+    rewrite /= defEZ abelianM (abelem_abelian abelemE) (abelem_abelian (Ohm1_abelem pZ _)) //=.
+    apply: (centSS sER (Ohm_sub _ _)). apply/centsP=> x Rx y. case/centerP=> Ry Cy.
+    by apply: commute_sym; apply Cy. 
+  move=> x; rewrite /= defEZ; case/mulsgP=> e z Ee Zz -> {x}.
+  have commz : z \in 'Z(R) := subsetP (Ohm_sub _ _) _ Zz.
+  have defzp : z^+p = 1.
+    have Gz : group_set [set x \in 'Z(R) | x ^+p == 1].
+      rewrite /group_set !inE !in_group exp1gn eqxx; apply/subsetP=> x.
+      case/mulsgP=> x1 x2; case/setIdP=> Zx1 xp1; case/setIdP=> Zx2 xp2 ->.
+      rewrite inE groupM // expMgn ?(eqP xp1) ?(eqP xp2) ?mulg1 ?eqxx //.
+      by case/centerP: Zx1=> Rx1; apply; apply: (subsetP (center_sub _)).
+    suff: z \in Group Gz by rewrite inE; case/andP=> _; move/eqP.
+    apply: (subsetP _ _ Zz); rewrite gen_subG; apply/subsetP=> x.
+    case/setIdP=> Zx xp /=; rewrite inE Zx -(eqP xp).
+    case: (eqVneq x 1); first by move ->; rewrite !exp1gn eqxx.
+    move=> x1; rewrite (pdiv_p_elt (mem_p_elt (pgroupS (center_sub _) pR) Zx) x1). 
+    by rewrite expn1 eqxx.
+  have ? : commute z e by case/centerP: commz=> _; apply; apply: (subsetP sER). 
+  by rewrite expMgn // defzp mulg1; case/(abelemP p_pr): abelemE=> _; apply.
+have defE : E * Z = E.
+  rewrite -defEZ maxE ?mulgen_subl //; rewrite inE abelemEZ mulgen_subG sER.
+  by rewrite (subset_trans (char_sub (Ohm_char 1 _))) ?center_sub.
+have sZE : Z \subset E by rewrite -defE mulg_subr.
+have rankCRE : 'r('C_R(E)%G) = 2.
+  by rewrite -rankE -rank_Ohm1 (Ohm_cent pmaxE pR). 
+have five1b : #| Z | = p.
+  case: (eqVneq 'Z(R) 1) => trivZ.
+    move: ntR; have defR := (trivg_center_pgroup pR trivZ). 
+    by case/negP; rewrite -(inj_eq (@group_inj _)) /= defR eqxx. 
+  apply:  Ohm1_cyclic_pgroup_prime; rewrite ?(pgroupS (center_sub _) pR) //.
+  (*
+  rewrite (odd_pgroup_rank1_cyclic pZ) ?(oddSg (center_sub _)) //=.
+
+  apply/eqP; rewrite eqn_leq -(odd_pgroup_rank1_cyclic pZ). p_rank_abelian //.
+  *)
+  admit.
+have five1a : Z \proper E.
+  rewrite properEcard sZE (logn_injcard1 (pgroupS _ pR)) ?abelem_pgroup //.
+     exact: subset_trans (Ohm_sub _ _) (center_sub _).
+  by rewrite -(rank_abelem abelemE) rankE five1b logn_prime // eqxx.
 admit.
 Qed.
 
