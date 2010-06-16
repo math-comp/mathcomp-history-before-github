e76b093339a8d2da5989b4d573861ecca447ccea
gareuselesinge <gareuselesinge@305884b8-be16-0410-aa53-a3be2363cef1>
2010-06-16 12:54:20 +0000

    10.1 and 10.2 stated
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/coqfinitgroup/trunk@2216 305884b8-be16-0410-aa53-a3be2363cef1

--- BGsection10.v
+++ BGsection10.v
@@ -38,7 +38,8 @@ Notation "\beta ( M )" := (beta M)
   (at level 2, format "\beta ( M )") : group_scope.
 
 Definition sigma M := 
-  [pred p \in \pi(#|M|) | existsb P, p.-Sylow(M) P && ('N_G(P) \subset M) ].
+  [pred p \in \pi(#|M|) | 
+     existsb P : {group gT}, p.-Sylow(M) P && ('N(P) \subset M) ].
 
 Notation "\sigma ( M )" := (sigma M)
   (at level 2, format "\sigma ( M )") : group_scope.
@@ -69,7 +70,7 @@ Proof. by move=> p; rewrite !inE; case/andP=> ->. Qed.
 Lemma alpha_sub_sigma : {subset \alpha(M) <= \sigma(M)}.
 Proof.
 move=> p; rewrite !inE; case/andP=> -> rM; have [P Syl_P] := Sylow_exists p M.
-apply/existsP; exists (gval P); rewrite Syl_P (subset_trans (subsetIr _ _)) //.
+apply/existsP; exists P; rewrite Syl_P.
 rewrite uniq_mmax_norm_sub // (def_uniq_mmax _ M_max) ?(pHall_sub Syl_P) //.
 have pPG := sub_proper_trans (pHall_sub Syl_P) M_proper.
 rewrite (p_rank_Sylow Syl_P) -(rank_pgroup (pHall_pgroup Syl_P)) in rM.
@@ -82,5 +83,89 @@ Proof. exact: sub_pcore beta_sub_alpha. Qed.
 Lemma Malpha_sub_Msigma : M`_\alpha \subset M`_\sigma.
 Proof. exact: sub_pcore alpha_sub_sigma. Qed.
 
+Implicit Type P X: {group gT}.
+
+Lemma norm_Sylow_sigma : 
+  forall p P, p \in \sigma(M) -> p.-Sylow(M) P -> 'N(P) \subset M.
+Proof.
+move=> p P; case/andP=> pdM; case/existsP=> Q; case/andP=> pSyl_Q sNPM pSyl_P.
+by case: (Sylow_trans pSyl_Q pSyl_P) => m mM ->; rewrite normJ conj_subG.
+Qed.
+
+Lemma Sylow_Sylow_sigma : 
+  forall p P, p \in \sigma(M) -> p.-Sylow(M) P -> p.-Sylow(G) P.
+Proof.
+move=> p P p_Sig pSyl_P; apply: (mmax_sigma_Sylow M_max) => //.
+exact: norm_Sylow_sigma p_Sig pSyl_P.
+Qed.
+
+Theorem BG10_1d : 
+    forall p X, p \in \sigma(M) -> X :!=: 1 -> p.-group X ->
+  p.-Sylow(M) X -> forall g, X :^ g \subset M -> g \in M.
+Proof.
+Admitted. 
+
+Let BG10b_to_a :
+    forall p X, p \in \sigma(M) -> X :!=: 1 -> p.-group X ->
+    [transitive 'C(X), on [set N | N <- orbit 'Js G M, X \subset N] | 'Js] ->
+    X \subset M -> forall g, X :^g \subset M -> 
+  exists c, exists m, [/\ c \in 'C(X),  m \in M & g = c * m].
+Admitted.
+
+Let BG10a_to_c :
+    forall p X, p \in \sigma(M) -> X :!=: 1 -> p.-group X ->
+    X \subset M -> 
+    (forall g, X :^g \subset M -> 
+      exists c, exists m, [/\ c \in 'C(X),  m \in M & g = c * m]) ->
+  'N(X) = 'N_M(X) * 'C(X). 
+Proof.
+Admitted.
+
+Theorem BG10b :
+    forall p X, p \in \sigma(M) -> X :!=: 1 -> p.-group X ->
+    [transitive 'C(X), on [set N | N <- orbit 'Js G M, X \subset N] | 'Js].
+Proof.
+Admitted.
+
+Theorem BG10a :
+    forall p X, p \in \sigma(M) -> X :!=: 1 -> p.-group X ->
+    X \subset M -> forall g, X :^g \subset M -> 
+  exists c, exists m, [/\ c \in 'C(X),  m \in M & g = c * m].
+Proof.
+move=> p X p_Sig ntX pX sXM g sXgM; apply: (BG10b_to_a _ _ pX) => //.
+exact: BG10b p_Sig ntX pX.
+Qed.
+
+Theorem BG10_c :
+    forall p X, p \in \sigma(M) -> X :!=: 1 -> p.-group X ->
+    X \subset M -> 'N(X) = 'N_M(X) * 'C(X). 
+Proof.
+move=> p X p_Sig ntX pX sXM; apply: (BG10a_to_c p_Sig ntX pX) => //.
+exact: BG10a p_Sig ntX pX sXM.
+Qed.
+
+Theorem BG10e :
+    forall p X, p \in \sigma(M) -> X :!=: 1 -> p.-group X ->
+  'C(X) \subset M -> forall g, X :^ g \subset M -> g \in M.
+Proof.
+Admitted.
+
+Theorem BG10_2a : \alpha(M).-Hall(M) M`_\alpha /\ \alpha(M).-Hall(G) M`_\alpha.
+Admitted.
+
+Theorem BG10_2b : \sigma(M).-Hall(M) M`_\sigma /\ \sigma(M).-Hall(G) M`_\sigma.
+Admitted.
+
+Theorem BG10_2c : M`_\sigma \subset M^`(1).
+Admitted.
+
+Theorem BG10_2d1 : 'r(M / M`_\alpha) <= 2. 
+Admitted.
+
+Theorem BG10_2d2 : nilpotent (M / M`_\alpha). 
+Admitted.
+
+Theorem BG10_2e : M`_\sigma :!=: 1.
+Admitted.
 
 End Ten.
