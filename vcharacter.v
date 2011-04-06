cc578b10af819b60d7b5c6ed8fee6f75173ce1da
thery <thery@305884b8-be16-0410-aa53-a3be2363cef1>
2011-04-06 17:06:40 +0000

    starting with virtual character
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/coqfinitgroup/trunk@2895 305884b8-be16-0410-aa53-a3be2363cef1

---
+++ vcharacter.v
@@ -0,0 +1,150 @@
+(* (c) Copyright Microsoft Corporation and Inria. All rights reserved. *)
+Require Import ssreflect ssrbool ssrfun eqtype ssrnat seq path div choice.
+Require Import fintype tuple finfun bigop prime ssralg poly finset.
+Require Import fingroup morphism perm automorphism quotient finalg action.
+Require Import zmodp commutator cyclic center pgroup sylow matrix mxalgebra.
+Require Import mxpoly mxrepresentation vector algC classfun character.
+
+Set Implicit Arguments.
+Unset Strict Implicit.
+Unset Printing Implicit Defensive.
+
+Import GroupScope GRing.Theory.
+Local Open Scope ring_scope.
+
+(**************************************************************************)
+(*                                                                        *)
+(* This file contains the basic notions of virtual character theory       *)
+(*                                                                        *)
+(*  is_vchar G f : predicates that tells if the function f is a virtual   *)
+(*                 character                                              *)
+(*                                                                        *)
+(**************************************************************************)
+
+Section IsVChar.
+
+Variable (gT : finGroupType).
+
+Section IsCharDef.
+
+Variable G : {set gT}.
+
+Definition is_vchar  (f : cfun gT algC) :=
+  all (fun theta : irr G => isZC (ncoord theta f)) (enum (irr G)) &&
+  (f \in 'CF[algC](<<G>>)).
+
+End IsCharDef.
+
+Variable G : {group gT}.
+
+Lemma is_vcharP : forall f,
+  reflect (exists f1, exists f2, [/\ is_char G f1, is_char G f2 & f = f1 - f2])
+          (is_vchar G f).
+Proof.
+move=> f; apply: (iffP andP); last first.
+  case=> f1; case=> f2 [Hf1 Hf2 ->]; rewrite genGid; split; last first.
+    by apply:  memv_sub; exact: is_char_in_cfun.
+  apply/allP=> chi _.
+  by rewrite linearD linearN isZC_sub // isZCE isNatC_ncoord_char.
+case; move/allP; rewrite genGid => HZ Hf.
+pose f1 := 
+  \sum_(i : irr G) (if isNatC(ncoord i f) then ncoord i f else 0) *: (i : cfun _ _).
+pose f2 := 
+  \sum_(i : irr G) (if isNatC(-ncoord i f) then -ncoord i f else 0) *: (i : cfun _ _).
+exists f1; exists f2; split.
+- apply: is_char_sum=> i _.
+  case: (boolP (isNatC _)); last by rewrite scale0r is_char0.
+  by case/isNatCP=> n ->; rewrite scaler_nat is_char_scal // is_char_irr.
+- apply: is_char_sum=> i _.
+  case: (boolP (isNatC _)); last by rewrite scale0r is_char0.
+  by case/isNatCP=> n ->; rewrite scaler_nat is_char_scal // is_char_irr.
+rewrite -sumr_sub {1}(ncoord_sum Hf); apply: eq_bigr=> chi _.
+have: isZC (ncoord chi f) by apply: HZ; rewrite mem_enum.
+rewrite isZCE; case/orP=> HH; rewrite HH; case: (boolP (isNatC _))=> HH1.
+- suff->: (ncoord chi f) = 0 by rewrite oppr0 !scale0r subrr.
+  apply: leC_anti; last by apply posC_isNatC.
+  by rewrite -leC_sub sub0r posC_isNatC.
+- by rewrite scale0r subr0.
+- suff->: (ncoord chi f) = 0 by rewrite oppr0 !scale0r subrr.
+  apply: leC_anti; last by apply posC_isNatC.
+  by rewrite -leC_sub sub0r posC_isNatC.
+by rewrite scale0r sub0r scaleNr opprK.
+Qed.
+
+Lemma is_vchar_char : forall f, is_char G f -> is_vchar G f.
+Proof.
+move=> f Hf; apply/is_vcharP; exists f; exists 0; split=> //.
+  by exact: is_char0.
+by rewrite subr0.
+Qed.
+
+Lemma is_vchar_irr : forall theta : irr G, is_vchar G theta.
+Proof.
+by move=> theta; apply: is_vchar_char; apply: is_char_irr.
+Qed.
+
+Lemma isZC_ncoord_vchar : forall (theta : irr G) (f : cfun _ _),
+ is_vchar G f -> isZC (ncoord theta f).
+Proof.
+move=> theta f; case/andP; move/allP=> HH _; apply: HH.
+by rewrite mem_enum.
+Qed.
+
+Lemma is_vchar0 : is_vchar G 0.
+Proof. by rewrite is_vchar_char // is_char0. Qed.
+
+Lemma is_vchar_in_cfun : forall f,  is_vchar G f -> f \in 'CF(G).
+Proof. 
+move=> f; case/is_vcharP=> f1; case=> f2 [Hf1 Hf2 ->].
+by apply: memv_sub; apply: is_char_in_cfun.
+Qed.
+
+Lemma is_vchar_opp : forall f, is_vchar G f -> is_vchar G (-f).
+Proof.
+move=> f; case/is_vcharP=> f1; case=> f2 [Hf1 Hf2 ->].
+by rewrite oppr_sub; apply/is_vcharP; exists f2; exists f1; split.
+Qed.
+
+Lemma is_vchar_add : forall f1 f2, 
+  is_vchar G f1 -> is_vchar G f2 -> is_vchar G (f1 + f2).
+Proof.
+move=> f1 f2; case/is_vcharP=> f11; case=> f12 [Hf11 Hf12 ->].
+case/is_vcharP=> f21; case=> f22 [Hf21 Hf22 ->].
+apply/is_vcharP; exists (f11 + f21); exists (f12 + f22); split.
+- by apply: is_char_add.
+- by apply: is_char_add.
+rewrite -!addrA; congr (_ + _).
+by rewrite addrC -!addrA oppr_add [-_ + _]addrC.
+Qed.
+
+Lemma is_vchar_sub : forall f1 f2, 
+  is_vchar G f1 -> is_vchar G f2 -> is_vchar G (f1 - f2).
+Proof.
+by move=> f1 f2 Hf1 Hf2; apply: is_vchar_add=> //; exact: is_vchar_opp.
+Qed.
+
+Lemma is_vchar_scal : forall f n, is_vchar G f -> is_vchar G (f *+ n).
+Proof.
+move=> f n Hf; elim: n=> [|n Hn]; first by rewrite mulr0n is_vchar0.
+by rewrite mulrS is_vchar_add.
+Qed.
+
+Lemma is_vchar_scaln : forall f n, is_vchar G f -> is_vchar G (f *- n).
+Proof.
+move=> f n Hf; elim: n=> [|n Hn]; first by rewrite oppr0 is_vchar0.
+by rewrite -mulNrn mulrS mulNrn is_vchar_add // is_vchar_opp.
+Qed.
+
+Lemma is_vchar_sum : forall (I : eqType) r (P : I -> bool) (F : I -> cfun _ _),
+  (forall i, ((P i) && (i \in r)) -> is_vchar G (F i)) -> 
+    is_vchar G (\sum_(i <- r | P i)  F i).
+Proof.
+move=> I r P F; elim: r=> [| a r IH HH].
+  by rewrite big_nil is_vchar0.
+have HF : is_vchar G (\sum_(i <- r | P i) F i).
+  by apply: IH=> i; case/andP=> HH1 HH2; apply: HH; rewrite HH1 inE HH2 orbT.
+rewrite big_cons; case: (boolP (P a))=> HP //; rewrite is_vchar_add //.
+by rewrite HH // inE eqxx andbT.
+Qed.
+
+End IsVChar.
