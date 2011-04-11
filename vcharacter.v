112faf12477409086c8b1a88ff9df81317928918
lrg <lrg@305884b8-be16-0410-aa53-a3be2363cef1>
2011-04-11 12:36:07 +0000

    removing is_vchar for Laurent, next step : generalization to Z[S , A]
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/coqfinitgroup/trunk@2924 305884b8-be16-0410-aa53-a3be2363cef1

--- vcharacter.v
+++ vcharacter.v
@@ -49,24 +49,22 @@ move=> f; apply: (iffP andP); last first.
     case/andP; move/eqP=> -> _.
     by apply: memv_sub; apply: is_char_in_cfun.
   apply/andP; split.
-    apply/forallP=> x.
-    rewrite linearD linearN /= ffunE isZC_add //.
+    apply/forallP=> x; rewrite linearD linearN /= ffunE isZC_add //.
       rewrite isZCE; apply/orP;left.
       by move: (isNatC_ncoord_char ( bi2sg x) Hf1);rewrite /ncoord  bi2sgK //=.
-    rewrite ffunE isZC_opp //; rewrite isZCE; apply/orP;left.
+    rewrite ffunE isZC_opp //; rewrite isZCE; apply/orP;left;
     by move: (isNatC_ncoord_char ( bi2sg x) Hf2);rewrite /ncoord  bi2sgK //=.
   apply/forallP=> x.
-  rewrite !cfunE; case:(boolP (x \in G)).
-    by move=> _;apply implybT.
+  rewrite !cfunE; case:(boolP (x \in G)); first by move=> _; apply: implybT.
   move=> HxG; move/is_char_in_cfun: (Hf1);move/cfun_memP.
   move=>[H _]; rewrite (H _ HxG).
   move/is_char_in_cfun: (Hf2);move/cfun_memP; move=>[H2 _].
   by rewrite (H2 _ HxG) subr0 implybF eqxx.
 case=> Hs; case/andP; move/forallP => Hc Hss.
-pose f1 := 
-  \sum_(i : irr G) (if isNatC(ncoord i f) then ncoord i f else 0) *: (i : cfun _ _).
- pose f2 := 
-  \sum_(i : irr G) (if isNatC(-ncoord i f) then -ncoord i f else 0) *: (i : cfun _ _).
+pose f1 := \sum_(i : irr G) (if isNatC(ncoord i f) then ncoord i f else 0) 
+                            *: (i : cfun _ _).
+pose f2 := \sum_(i : irr G) (if isNatC(-ncoord i f) then -ncoord i f else 0) 
+                            *: (i : cfun _ _).
 exists f1; exists f2; split.
 - apply: is_char_sum=> i _.
   case: (boolP (isNatC _)); last by rewrite scale0r is_char0.
@@ -76,8 +74,7 @@ exists f1; exists f2; split.
   by case/isNatCP=> n ->; rewrite scaler_nat is_char_scal // is_char_irr.
 rewrite -sumr_sub.
 have Hf : f \in 'CF(G).
-   case/andP: (base_irr_basis G) Hs; rewrite /is_basis  /is_span.
-   by move/eqP => ->.
+   by case/andP: (base_irr_basis G) Hs; rewrite /is_span;move/eqP => ->.
 rewrite {1}(ncoord_sum Hf); apply: eq_bigr=> chi _.
 have: isZC (ncoord chi f) by apply: Hc; rewrite mem_enum.
 rewrite isZCE; case/orP=> HH; rewrite HH; case: (boolP (isNatC _))=> HH1.
@@ -91,42 +88,41 @@ rewrite isZCE; case/orP=> HH; rewrite HH; case: (boolP (isNatC _))=> HH1.
 by rewrite scale0r sub0r scaleNr opprK.
 Qed.
 
-Definition  is_vchar G f  :=(f \in 'Z['Irr G]).
-Lemma is_vchar_char : forall f, is_char G f -> is_vchar G f.
+Lemma is_vchar_char : forall f, is_char G f -> f \in 'Z['Irr G].
 Proof.
 move=> f Hf; apply/is_vcharP; exists f; exists 0; split=> //.
   by exact: is_char0.
 by rewrite subr0.
 Qed.
 
-Lemma is_vchar_irr : forall theta : irr G, is_vchar G theta.
+Lemma is_vchar_irr : forall theta : irr G, (theta: cfun _ _) \in 'Z['Irr G].
 Proof.
 by move=> theta; apply: is_vchar_char; apply: is_char_irr.
 Qed.
 
 Lemma isZC_ncoord_vchar : forall (theta : irr G) (f : cfun _ _),
- is_vchar G f -> isZC (ncoord theta f).
+ f \in 'Z['Irr G] -> isZC (ncoord theta f).
 Proof.
 by move=> theta f; case/andP => Hf; case/andP; move/forallP=> HH _; apply: HH.
 Qed.
 
-Lemma is_vchar0 : is_vchar G 0.
+Lemma is_vchar0 :  0 \in 'Z['Irr G].
 Proof. by rewrite is_vchar_char // is_char0. Qed.
 
-Lemma is_vchar_in_cfun : forall f,  is_vchar G f -> f \in 'CF(G).
+Lemma is_vchar_in_cfun : forall f, f \in 'Z['Irr G]-> f \in 'CF(G).
 Proof. 
 move=> f; case/is_vcharP=> f1; case=> f2 [Hf1 Hf2 ->].
 by apply: memv_sub; apply: is_char_in_cfun.
 Qed.
 
-Lemma is_vchar_opp : forall f, is_vchar G f -> is_vchar G (-f).
+Lemma is_vchar_opp : forall f, f \in 'Z['Irr G]-> (-f) \in 'Z['Irr G].
 Proof.
 move=> f; case/is_vcharP=> f1; case=> f2 [Hf1 Hf2 ->].
 by rewrite oppr_sub; apply/is_vcharP; exists f2; exists f1; split.
 Qed.
 
 Lemma is_vchar_add : forall f1 f2, 
-  is_vchar G f1 -> is_vchar G f2 -> is_vchar G (f1 + f2).
+  f1 \in 'Z['Irr G]-> f2  \in 'Z['Irr G]-> (f1 + f2) \in 'Z['Irr G].
 Proof.
 move=> f1 f2; case/is_vcharP=> f11; case=> f12 [Hf11 Hf12 ->].
 case/is_vcharP=> f21; case=> f22 [Hf21 Hf22 ->].
@@ -138,30 +134,30 @@ by rewrite addrC -!addrA oppr_add [-_ + _]addrC.
 Qed.
 
 Lemma is_vchar_sub : forall f1 f2, 
-  is_vchar G f1 -> is_vchar G f2 -> is_vchar G (f1 - f2).
+   f1  \in 'Z['Irr G]  -> f2 \in 'Z['Irr G] -> (f1 - f2) \in 'Z['Irr G]. 
 Proof.
 by move=> f1 f2 Hf1 Hf2; apply: is_vchar_add=> //; exact: is_vchar_opp.
 Qed.
 
-Lemma is_vchar_scal : forall f n, is_vchar G f -> is_vchar G (f *+ n).
+Lemma is_vchar_scal : forall f n, f \in 'Z['Irr G] -> (f *+ n) \in 'Z['Irr G].
 Proof.
 move=> f n Hf; elim: n=> [|n Hn]; first by rewrite mulr0n is_vchar0.
 by rewrite mulrS is_vchar_add.
 Qed.
 
-Lemma is_vchar_scaln : forall f n, is_vchar G f -> is_vchar G (f *- n).
+Lemma is_vchar_scaln : forall f n, f \in 'Z['Irr G] -> (f *- n) \in 'Z['Irr G].
 Proof.
 move=> f n Hf; elim: n=> [|n Hn]; first by rewrite oppr0 is_vchar0.
 by rewrite -mulNrn mulrS mulNrn is_vchar_add // is_vchar_opp.
 Qed.
 
 Lemma is_vchar_sum : forall (I : eqType) r (P : I -> bool) (F : I -> cfun _ _),
-  (forall i, ((P i) && (i \in r)) -> is_vchar G (F i)) -> 
-    is_vchar G (\sum_(i <- r | P i)  F i).
+  (forall i, ((P i) && (i \in r)) -> (F i) \in 'Z['Irr G]) -> 
+    (\sum_(i <- r | P i)  F i) \in 'Z['Irr G].
 Proof.
 move=> I r P F; elim: r=> [| a r IH HH].
   by rewrite big_nil is_vchar0.
-have HF : is_vchar G (\sum_(i <- r | P i) F i).
+have HF : (\sum_(i <- r | P i) F i) \in 'Z['Irr G].
   by apply: IH=> i; case/andP=> HH1 HH2; apply: HH; rewrite HH1 inE HH2 orbT.
 rewrite big_cons; case: (boolP (P a))=> HP //; rewrite is_vchar_add //.
 by rewrite HH // inE eqxx andbT.
@@ -175,8 +171,17 @@ Section MoreIsVChar.
 Variable gT : finGroupType.
 Variable G H : {group gT}.
 
+Local Notation " 'Z[ S , A ]" := 
+  (virtual_char_pred S A) (format " ''Z[' S ,  A ]"). 
+
+Local Notation " 'Z[ 'Irr G , A ]" := 
+  (virtual_char_pred (base_irr G) A) (format "''Z[' ''Irr'  G ,  A ]"). 
+
+Local Notation " 'Z[ 'Irr G ]" := 
+  (virtual_char_pred (base_irr G) G) (format "''Z[' ''Irr'  G ]"). 
+
 Lemma is_vchar_restrict : forall f, 
-  H \subset G -> is_vchar G f -> is_vchar H ('Res[H] f).
+  H \subset G -> f \in 'Z['Irr G] -> ('Res[H] f)\in 'Z['Irr H].
 Proof.
 move=> f HsG; case/is_vcharP=> f1; case=> f2 [Hf1 Hf2 ->].
 by rewrite linearD linearN is_vchar_sub // is_vchar_char //
@@ -184,7 +189,7 @@ by rewrite linearD linearN is_vchar_sub // is_vchar_char //
 Qed.
 
 Lemma is_vchar_induced : forall chi,
-   H \subset G -> is_vchar H chi -> is_vchar G ('Ind[G,H] chi).
+   H \subset G -> chi \in 'Z['Irr H] -> ('Ind[G,H] chi) \in 'Z['Irr G].
 Proof.
 move=> chi HsG; case/is_vcharP=> f1; case=> f2 [Hf1 Hf2 ->].
 by rewrite linearD linearN is_vchar_add ?is_vchar_opp //
