91b8b5fdc131b8746d97b69a94fa6cc865f8010a
thery <thery@305884b8-be16-0410-aa53-a3be2363cef1>
2011-06-08 07:01:24 +0000

    induction is an isometry (Isaacs' 7.7)
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/coqfinitgroup/trunk@3214 305884b8-be16-0410-aa53-a3be2363cef1

--- PFsection3.v
+++ PFsection3.v
@@ -24,6 +24,77 @@ Import Prenex Implicits.
 Import GroupScope GRing.Theory.
 Local Open Scope ring_scope.
 
+(* This is Isaacs' 7.7 *)
+Section TiInducedIso.
+
+Variables (gT : finGroupType) (G : {group gT}) (X: {set gT}).
+Variables (phi theta : {cfun gT}).
+
+Hypothesis TiX :  trivIset (X^# :^: G).
+Hypothesis Cphi : phi \in 'CF('N_G(X), X).
+Hypothesis Ctheta : theta \in 'CF('N_G(X), X).
+Hypothesis theta1 : theta 1%g = 0.
+
+Lemma ti_restrict_induced : 'Res[X] ('Ind[G, 'N_G(X)] theta) = theta.
+Proof.
+apply/ffunP=> g; case: (boolP (g \in X))=> [GiX|GniX]; last first.
+  by rewrite 2!ffunE (negPf GniX) mul0r (cfunS0 Ctheta).
+rewrite 2!ffunE GiX mul1r.
+rewrite !ffunE (bigID (fun g => g \in 'N_G(X))) /=.
+rewrite (eq_bigr (fun i : gT => theta g))=> [/= | h]; last first.
+  by case/andP=> HiG HiN; rewrite (cfunJ _ Ctheta).
+rewrite sumr_const big1 ?addr0=> [| h].
+  set u := #|_|; suff->: u = #|'N_G(X)|.
+    by rewrite mulrC -[theta g *+ _]mulr_natr mulfK // neq0GC.
+  apply: eq_card=> i; rewrite [X in X = _]/= {1}/in_mem /=.
+  by case: (boolP (i \in _))=> //; rewrite inE; move/negPf->.
+case/andP=> HiG HiN.
+case: (boolP (g^h == 1%g))=> [/eqP-> //| Dgh].
+rewrite (cfunS0 Ctheta) //.
+apply/negP=> GHniX.
+suff: [disjoint X^# & (X :^ h)^#].
+  move/pred0P; move/(_ (g^h)%g)=> /=; rewrite !inE Dgh GHniX /=.
+  by move/idP; case; apply/imsetP; exists g.
+rewrite -conjD1g (trivIsetP TiX) //.
+- by apply/imsetP; exists 1%g; [exact: group1| rewrite conjsg1].
+- by apply/imsetP; exists h.
+rewrite conjD1g;  apply/eqP=> HH1; case/negP: HiN.
+rewrite !inE HiG /=.
+apply/subsetP=> k.
+case/imsetP=> k1 K1iX ->.
+case: (boolP (k1 == 1%g)) K1iX=> [/eqP-> | HH2 HH3].
+  by rewrite conj1g.
+have[/subsetP]: X^# \subset X by apply/subsetP=> x; rewrite !inE; case/andP.
+apply; rewrite HH1 !inE; apply/andP; split; last first.
+  by apply/imsetP; exists k1.
+apply/eqP=> HH4; case/eqP: HH2.
+by rewrite -(conj1g (h^-1)) -HH4 -conjgM mulgV conjg1.
+Qed.
+
+Lemma ti_induced_iso : 
+ '['Ind[G, 'N_G(X)] theta, 'Ind[G, 'N_G(X)] phi]_G = '[theta, phi]_'N_G(X).
+Proof.
+have NsG: 'N_G(X) \subset G.
+  by apply/subsetP=> h; rewrite inE => /andP [].
+rewrite inner_conj -frobenius_reciprocity -?inner_conj //; last first.
+- by apply: memc_induced=> //; apply: memcW Ctheta.
+- by apply: memcW Cphi.
+apply/eqP; rewrite -subr_eq0 /=; apply/eqP.
+ (* why this does not work rewrite -!inner_prodbE -linear_sub ? *)
+rewrite -!inner_prodbE;  set u := inner_prodb _ _.
+have Fu : linear (u : _ -> algC^o) by apply: inner_prodb_is_linear.
+move: (linear_sub (Linear Fu))=> /= <-; rewrite /u inner_prodbE.
+rewrite inner_prodE big1 ?mulr0 // => g GiNG.
+case: (boolP (g \in X))=> [GiX | GniX]; last first.
+  by rewrite (cfunS0 Cphi) ?(ffunE,conjC0,mulr0,GniX,GiNG,inE).
+rewrite 3!ffunE GiNG mul1r.
+move/ffunP: ti_restrict_induced; move/(_ g).
+rewrite 2!ffunE GiX mul1r => ->.
+by rewrite !ffunE subrr mul0r.
+Qed.
+
+End TiInducedIso.
+
 Section Definitions.
 
 Variables (gT : finGroupType) (G W W1 W2 Wi : {set gT}).
