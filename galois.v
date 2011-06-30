36d7c7ecca3ed56f272a2cf3f748c0304101ba59
roconnor <roconnor@305884b8-be16-0410-aa53-a3be2363cef1>
2011-06-30 13:06:20 +0000

    Removing dead proofs.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/coqfinitgroup/trunk@3281 305884b8-be16-0410-aa53-a3be2363cef1

--- galois.v
+++ galois.v
@@ -905,20 +905,6 @@ case/memv_imgP: Ha => [b [Hb ->]].
 by rewrite -[X in X \in _]comp_lappE inv_lker0 ?unit_lappE // LAut_ker0.
 Qed.
 
-(*
-Lemma Aut_group_set : forall K E : {algebra L}, 
-  group_set ('Aut( E | K ))%g.
-Proof.
-move => K E.
-rewrite /Aut.
-apply/isgroupP.
-by exists (group (kAut_group_set K E) / 
-            [set x : LAut | kHom E (fullv L) (val x)])%G.
-Qed.
-
-Notation "''Aut' ( E | K )" := (group (Aut_group_set K E)) : subgroup_scope.
-*)
-
 (* Move this to vector.v *)
 Lemma eqvP (V : {vspace L}) (f g: 'End(L)) :
   reflect (forall a, a \in V -> f a = g a)
@@ -1082,14 +1068,6 @@ move/implyP/(_ HKEf)/forallP/(_ g)/implyP => Hpick Hg.
 apply/eqvP.
 by apply: Hpick.
 Qed.
-
-(* Test our ability to state parts of Galois's great theorem. *)
-(*
-Hypothesis foo : forall (E Q K: {algebra L}), (K <= Q)%VS ->
-  ('Aut (E | Q) \subset 'Aut (E | K))%g.
-Hypothesis bar : forall (E Q K: {algebra L}),
-  ('Aut (Q | K) \isog ('Aut (E | K) / 'Aut (E | Q)))%G.
-*)
 
 Definition FixedField (E : {vspace L})
   (s : {set coset_of [set x : LAut | kAut E (fullv L) (val x)]}) :=
@@ -1758,153 +1736,4 @@ Qed.
 
 End GaloisDim.
 
-
-(*
-Definition FieldAutomorphism (E:{vspace L}) (f : 'End(L) ) : bool :=
- [&& (f @: E == E)%VS, (E^C <= eigenspace f 1)%VS &
- (all (fun v1 => all (fun v2 => f (v1 * v2) == f v1 * f v2) (vbasis E))
-      (vbasis E))].
-
-Definition FixedField E f := (E :&: @eigenspace F0 L f 1)%VS.
-
-Implicit Types (E K:{algebra L}).
-
-Lemma AutomorphMul : forall E f, FieldAutomorphism E f -> 
-forall u v, u \in E -> v \in E -> f (u * v) = f u * f v.
-Proof.
-move => E f.
-case/and3P => _ _.
-move/all_nthP => fmult u v.
-move/coord_basis => -> Hv.
-rewrite -mulr_suml !linear_sum -mulr_suml.
-apply: eq_bigr => i _.
-move/coord_basis: Hv ->.
-rewrite -mulr_sumr !linear_sum.
-apply: eq_bigr => j _ /=.
-rewrite ![f (_ *: _)]linearZ -scaler_mull linearZ -scaler_mulr linearZ.
-rewrite -scaler_mull -scaler_mulr.
-do 2 apply: f_equal.
-apply/eqP.
-move: (ltn_ord i).
-rewrite -{2}(size_tuple (vbasis _)).
-move/(fmult 0 _).
-move/all_nthP.
-apply.
-by rewrite size_tuple.
-Qed.
-
-Lemma AutomorphismIsUnit : forall E f, 
- FieldAutomorphism E f -> lker f = 0%:VS.
-Proof.
-move => E f.
-case/and3P => Hf1 Hf2 _.
-apply/eqP.
-rewrite -(capfv (lker f)) -dimv_eq0 -(eqn_addr (\dim (f @: (fullv _)))).
-rewrite add0n limg_ker_dim -(addv_complf E) limg_add.
-move/eqP: Hf1 ->.
-rewrite (eigensubspaceImageEq _ Hf2) //.
-apply: nonzero1r.
-Qed.
-
-Lemma idAutomorphism : forall E, FieldAutomorphism E \1%VS.
-Proof.
-move => E.
-apply/and3P; split; do ? (apply/allP => i _ ;apply/allP => j _);
- rewrite ?unit_lappE ?lim1g //.
-apply/subvP => v _.
-by rewrite eigenspaceIn unit_lappE scale1r.
-Qed.
-
-Lemma compAutomorphism : forall E f g, 
- FieldAutomorphism E f ->  FieldAutomorphism E g ->
- FieldAutomorphism E (f \o g)%VS.
-Proof.
-move => E f g Ff Fg.
-case/and3P: (Ff); move/eqP => Hf1; move/subvP => Hf2; move/eqP => _.
-case/and3P: (Fg); move/eqP => Hg1; move/subvP => Hg2; move/eqP => _.
-apply/and3P; split; do 2? apply/allP => ? ?;
-  rewrite ?limg_comp ?comp_lappE /= ?Hg1 ?Hf1 //.
-  apply/subvP => v Hv.
-  rewrite eigenspaceIn comp_lappE /=.
-  move/(_ _ Hv): Hg2; rewrite eigenspaceIn; move/eqP->.
-  move/(_ _ Hv): Hf2; rewrite eigenspaceIn.
-  by rewrite  GRing.scale1r.
-by rewrite (AutomorphMul Fg);
- first (rewrite (AutomorphMul Ff) // -[g _ \in E]/(g _ \in a2vs E) -Hg1);
- do ? apply memv_img; apply: memv_basis.
-Qed.
-
-Lemma invAutomorphism : forall E f,
- FieldAutomorphism E f -> FieldAutomorphism E (f \^-1)%VS.
-Proof.
-move => E f Ff.
-have kerf := AutomorphismIsUnit Ff.
-case/and3P: (Ff); move/eqP => Hf1; move/subvP => Hf2; move/eqP => _.
-have H1 : (f \^-1 @: E = E)%VS.
- apply/eqP.
- by rewrite -{1}Hf1 -limg_comp inv_lker0 ?kerf // lim1g.
-apply/and3P; split.
-- by rewrite H1.
-- apply/subvP => v.
-  move/Hf2=> Hv; rewrite eigenspaceIn GRing.scale1r in Hv.
-  rewrite eigenspaceIn scale1r.
-  rewrite -{1}(eqP Hv) /=.
-  by rewrite -[(f \^-1)%VS (f _)](comp_lappE) inv_lker0 ?kerf // unit_lappE.
-- rewrite <- Hf1.
-  do 2 (apply/allP => ?; move/memv_basis;
-        case/memv_imgP => ? [? ->]).
-  rewrite -(AutomorphMul Ff) // -!{1}[(f \^-1)%VS (f _)](comp_lappE).
-  by rewrite inv_lker0 ?kerf // !unit_lappE.
-Qed.
-
-Lemma FixedField_is_subfield : forall E f,
-  FieldAutomorphism E f ->
-  let K0 := FixedField E f in
-  (has_aunit K0 && (K0 * K0 <= K0))%VS.
-Proof.
-move => E f Hf /=.
-apply/andP; split.
- rewrite has_aunit1 // memv_cap.
- apply/andP; split.
-  by apply: memv1.
- rewrite eigenspaceIn.
- apply/eqP.
- have Hf1 : f 1 != 0.
-  by rewrite -memv_ker (AutomorphismIsUnit Hf) memv0 nonzero1r.
- apply: (mulfI Hf1).
- by rewrite -(AutomorphMul Hf) ?memv1 // scale1r !mulr1.
-apply/prodvP => u v.
-rewrite !memv_cap !eigenspaceIn !scale1r.
-case/andP => uE; move/eqP => fu.
-case/andP => vE; move/eqP => fv.
-apply/andP; split.
- by rewrite memv_mul.
-by rewrite (AutomorphMul Hf) // fu fv.
-Qed.
-
-Canonical Structure FixedField_subfield E f Hf : {algebra L} :=
- Eval hnf in ASpace (@FixedField_is_subfield E f Hf).
-
-Lemma AutomorphsimEE_id : 
- forall E f, FieldAutomorphism E f -> FixedField E f = E -> f = \1%VS.
-Proof.
-move => E f.
-case/and3P => _.
-move/subvP => Hf _ HE.
-apply/eqP.
-apply/eq_lapp => v.
-move: (memvf v).
-rewrite unit_lappE -(addv_complf (FixedField E f)).
-move/memv_addP => [va [vb [Hva Hvb ->]]].
-move: Hva Hvb.
-rewrite memv_cap HE.
-rewrite linearD eigenspaceIn /=.
-case/andP => _.
-move/eqP ->.
-move/Hf.
-rewrite eigenspaceIn.
-move/eqP ->.
-by rewrite !scale1r.
-Qed.
-*)
 End Galois.
