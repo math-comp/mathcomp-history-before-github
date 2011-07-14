ecaefd25e8efbb82eefce4f53a43760a96e1c122
roconnor <roconnor@305884b8-be16-0410-aa53-a3be2363cef1>
2011-07-14 13:48:37 +0000

    Second draft of Hypotheses for Appendix C.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/coqfinitgroup/trunk@3318 305884b8-be16-0410-aa53-a3be2363cef1

--- BGappendixC.v
+++ BGappendixC.v
@@ -1,5 +1,6 @@
 Require Import ssreflect ssrbool ssrfun eqtype ssrnat div.
-Require Import fintype finfun ssralg finalg finset fingroup abelian morphism.
+Require Import fintype finfun ssralg finalg finset fingroup morphism.
+Require Import abelian frobenius.
 
 Set Implicit Arguments.
 Unset Strict Implicit.
@@ -13,13 +14,25 @@ Section AppendixC.
 Variables p q : nat.
 Hypothesis Hpq : ~~ (q %| p.-1).
 
-Variable G : finGroupType.
-Variables P U : {group G}.
+Variable gT : finGroupType.
+Variables G P U : {group gT}.
+Hypothesis HGPU : [Frobenius G = P ><| U]%g.
+Hypothesis Pcard : #|P| = p ^ q.
+Hypothesis Ucard : (#|U| * p.-1)%N = (p ^ q).-1.
+
+Variable Q : {group gT}.
+Hypothesis HQ : Q \in ('E_q(G))%g.
+
+Variable W2 : {group gT}. 
+Hypothesis HFpQ : W2 \subset 'N(Q)%g.
+
+Variable y : gT.
+Hypothesis HyQ : y \in Q.
+Hypothesis HFpUy : W2 \subset 'N(U :^ y)%g.
 
 CoInductive finFieldImage : Type :=
   FinFieldImage : forall (F : finFieldType) (sigma : {morphism P >-> F}%g)
-                         (tau : G -> F), #|F| = p^q -> isom P [set: F] sigma ->
-                         (#|tau @: U| * p.-1)%N = (p ^ q).-1 ->
+                         (tau : gT -> F), isom P [set: F] sigma ->
                          (forall p u, p \in P -> u \in U -> 
                                       sigma (p ^ u)%g = tau u * sigma p) -> 
                          finFieldImage.
@@ -27,25 +40,17 @@ CoInductive finFieldImage : Type :=
 Variable FSigmaTau : finFieldImage.
 
 Let F : finFieldType :=
-  let: FinFieldImage F _ _ _ _ _ _ := FSigmaTau in F.
+  let: FinFieldImage F _ _ _ _ := FSigmaTau in F.
 
 Let sigma : {morphism P >-> F}%g :=
-  let (F,sigma,_,_,_,_,_) as FST
-  return {morphism P >-> ((let: FinFieldImage F _ _ _ _ _ _ := FST in F)
+  let (F,sigma,_,_,_) as FST
+  return {morphism P >-> ((let: FinFieldImage F _ _ _ _ := FST in F)
                           : finFieldType)}%g
   := FSigmaTau in sigma.
 
 Let Fp := <[(1%R : F)]>%g.
 
-Let W2 := sigma @^-1: Fp.
-
-Variable Q : {group G}.
-Hypothesis HQPU : Q \in ('E_q(P <*> U))%g.
-Hypothesis HFpQ : W2 \subset 'N(Q)%g.
-
-Variable y : G.
-Hypothesis HyQ : y \in Q.
-Hypothesis HFpUy : W2 \subset 'N(U :^ y)%g.
+Hypothesis HW2Fp : sigma @: W2 = Fp.
 
 (* Lemma appendixC : p <= q. *)
 
