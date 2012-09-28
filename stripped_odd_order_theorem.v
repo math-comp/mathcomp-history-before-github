bc4380829d9e86fc901b59e4a4817d531876b618
gonthier <gonthier@305884b8-be16-0410-aa53-a3be2363cef1>
2012-09-28 11:27:28 +0000

    More beauty tweeks.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/coqfinitgroup/trunk@4141 305884b8-be16-0410-aa53-a3be2363cef1

--- stripped_odd_order_theorem.v
+++ stripped_odd_order_theorem.v
@@ -19,18 +19,18 @@ Inductive equal T (x : T) : T -> Type := Equal : equal T x x.
 
 (* Arithmetic *)
 
-Inductive natural : Type := Zero | Add_1_to (n : natural).
+Inductive natural := Zero | Add_1_to (n : natural).
 
 Fixpoint add (m n : natural) : natural :=
   match m with Zero => n | Add_1_to m_minus_1 => add m_minus_1 (Add_1_to n) end.
 
 Definition double (n : natural) : natural := add n n.
 
-Inductive odd (n : natural) : Type :=
+Inductive odd (n : natural) :=
   Odd (half : natural)
     (n_odd : equal natural n (Add_1_to (double half))).
 
-Inductive less_than (m n : natural) : Type :=
+Inductive less_than (m n : natural) :=
   LessThan (diff : natural)
     (m_lt_n : equal natural n (Add_1_to (add m diff))).
 
@@ -65,7 +65,7 @@ Inductive group T mul one inv (G : T -> Type) :=
 Inductive subgroup T mul one inv (H G : T -> Type) :=
   Subgroup
     (H_group    : group T mul one inv H)
-    (H_subset_G : forall x : T, H x -> G x).
+    (H_subset_G : forall x, H x -> G x).
 
 Inductive normal_subgroup T mul one inv (H G : T -> Type) :=
   NormalSubgroup
